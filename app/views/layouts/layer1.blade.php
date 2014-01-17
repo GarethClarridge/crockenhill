184e1e9e0c87d6d6a7525f4592df23621e752b4b
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2014-01-17 23:25:35 +0000

    Added header background image

--- app/views/layouts/layer1.blade.php
+++ app/views/layouts/layer1.blade.php
@@ -5,12 +5,16 @@
 <div class="row">
     <div class="col-md-9">
         <article class="card">
-            <ol class="breadcrumb">
-                <li> {{ link_to_route('Home', 'Home') }} </li>
-                @yield('breadcrumbs')
-            </ol>
             @yield('header')
-            @yield('main-content')
+            <div class="links">
+                <ol class="breadcrumb">
+                    <li> {{ link_to_route('Home', 'Home') }} </li>
+                    @yield('breadcrumbs')
+                </ol>
+            </div>
+            <div class="content">
+                @yield('main-content')
+            </div>
         </article>
     </div>
     <div class="col-md-3">
