07c555dfb3cadd1c34e5f83805bf0d66741fe6e9
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-01-28 23:15:11 +0000

    Added in permanent redirects

--- app/routes.php
+++ app/routes.php
@@ -103,6 +103,81 @@ Route::group(array('prefix' => 'members', 'before' => 'auth.member'), function()
     Route::get('/{slug}', array('uses' => 'PageController@showMemberPage'));
 });
 
+// Permanent Redirects
+
+Route::get('aboutus', function(){
+    return Redirect::to('about-us', 301);
+});
+Route::get('abouts', function(){
+    return Redirect::to('about-us', 301);
+});
+Route::get('contactus', function(){
+    return Redirect::to('contact-us', 301);
+});
+Route::get('links', function(){
+    return Redirect::to('about-us/links', 301);
+});
+Route::get('whatson', function(){
+    return Redirect::to('whats-on', 301);
+});
+Route::get('where', function(){
+    return Redirect::to('find-us', 301);
+});
+Route::get('aboutus/history', function(){
+    return Redirect::to('about-us/history', 301);
+});
+Route::get('aboutus/pastor', function(){
+    return Redirect::to('about-us/pastor', 301);
+});
+Route::get('aboutus/statementoffaith', function(){
+    return Redirect::to('about-us/statement-of-faith', 301);
+});
+Route::get('aboutus/whatwebelieve', function(){
+    return Redirect::to('about-us/what-we-believe', 301);
+});
+Route::get('whatson/1150', function(){
+    return Redirect::to('whats-on/1150', 301);
+});
+Route::get('whatson/adventurers', function(){
+    return Redirect::to('whats-on/adventurers', 301);
+});
+Route::get('whatson/babytalk', function(){
+    return Redirect::to('whats-on/baby-talk', 301);
+});
+Route::get('whatson/biblestudy', function(){
+    return Redirect::to('whats-on/bible-study', 301);
+});
+Route::get('whatson/buzzclub', function(){
+    return Redirect::to('whats-on/buzz-club', 301);
+});
+Route::get('whatson/carolsatthechequers', function(){
+    return Redirect::to('whats-on/carols-in-the-chequers', 301);
+});
+Route::get('whatson/christianityexplored', function(){
+    return Redirect::to('whats-on/christianity-explored', 301);
+});
+Route::get('whatson/coffeecup', function(){
+    return Redirect::to('whats-on/coffee-cup', 301);
+});
+Route::get('whatson/familyfunnight', function(){
+    return Redirect::to('whats-on/family-fun-night', 301);
+});
+Route::get('whatson/link', function(){
+    return Redirect::to('whats-on/link', 301);
+});
+Route::get('whatson/menzone', function(){
+    return Redirect::to('whats-on/menzone', 301);
+});
+Route::get('whatson/sunday', function(){
+    return Redirect::to('whats-on/sunday-services', 301);
+});
+Route::get('whatson/walkinggroup', function(){
+    return Redirect::to('whats-on/walking-group', 301);
+});
+Route::get('publications', function(){
+    return App::abort(404);
+});
+
 // General Routes
 
 Route::get('/{area}/{slug}', array('uses' => 'PageController@showSubPage'));
