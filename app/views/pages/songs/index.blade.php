5b13d3753d238a320e6b69a111a69a5c09b7ccc9
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-02-19 21:58:12 +0000

    Implemented praise number thumbnail

--- app/views/pages/songs/index.blade.php
+++ app/views/pages/songs/index.blade.php
@@ -3,27 +3,33 @@
 @section('dynamic_content')
 
   @foreach ($songs as $song)
-    <h3>{{$song->title}}</h3>
-    @if ($song->praise_number)
-      <p>
-        <span class="glyphicon glyphicon-music"></span> &nbsp
-        Praise Number: {{ $song->praise_number }}
-      </p>
-    @endif
+    <div class="media song">
+      @if ($song->praise_number)
+        <div class="media-left media-middle praise-icon">
+          <img class="media-object" src="/images/praise.png" alt="">
+          <span class="praise-number">{{ $song->praise_number }}</span>
+        </div>
+      @endif
 
-    @if ($song->author)
-      <p>
-        <span class="glyphicon glyphicon-user"></span> &nbsp
-        {{ $song->author }}
-      </p>
-    @endif
+      <div class="media-body media-middle song-body">
+        <h3 class="media-heading">
+          {{$song->title}}
+        </h3>
+        @if ($song->author)
+          <p>
+            <span class="glyphicon glyphicon-user"></span> &nbsp
+            {{ $song->author }}
+          </p>
+        @endif
 
-    @if ($song->copyright)
-      <p>
-        <span class="glyphicon glyphicon-copyright-mark"></span> &nbsp
-        {{ $song->copyright }}
-      </p>
-    @endif
+        @if ($song->copyright)
+          <p>
+            <span class="glyphicon glyphicon-copyright-mark"></span> &nbsp
+            {{ $song->copyright }}
+          </p>
+        @endif
+      </div>
+    </div>
 
   @endforeach
 
