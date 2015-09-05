f835121da95e37f6cc1b36d034764dd2e3351ed4
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-09-05 16:22:52 +0100

    Everything works, but members area not protected

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,21 @@
-@extends('app')
+@extends('page', 
+	['heading' => 'Log in', 
+	'description' => '<meta name="description" content="Log in to the member area">']
+	)
 
 @section('content')
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
+		<br><br><br>
+			<article class="card">
+        <div class="header-container">
+            <h1>Log in</h1>
+        </div>
+				<div>
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<strong>Whoops!</strong> There were some problems:<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
@@ -22,7 +28,7 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<label class="col-md-4 control-label">Email Address</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
@@ -49,12 +55,13 @@
 							<div class="col-md-6 col-md-offset-4">
 								<button type="submit" class="btn btn-primary">Login</button>
 
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+								<a class="btn btn-link" href="{{ url('/members/password/email') }}">Forgot Your Password?</a>
 							</div>
 						</div>
 					</form>
 				</div>
-			</div>
+			</article>
+			<br><br>
 		</div>
 	</div>
 </div>
