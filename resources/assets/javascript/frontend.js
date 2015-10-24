7f8ea206e15befcb16101dfa938a0d94cefdc79f
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-10-24 17:54:20 +0100

    Sermon upload autopopulate

--- resources/assets/javascript/frontend.js
+++ resources/assets/javascript/frontend.js
@@ -1,11 +1,11 @@
 $(function() {
  
-        // Confirm deleting resources
-        $("form[data-confirm]").submit(function() {
-                if ( ! confirm($(this).attr("data-confirm"))) {
-                        return false;
-                }
-        });
+	// Confirm deleting resources
+	$("form[data-confirm]").submit(function() {
+		if ( ! confirm($(this).attr("data-confirm"))) {
+			return false;
+		}
+	});
  
 });
 
@@ -18,54 +18,54 @@ var cookieName = 'complianceCookie';        // Name of our cookie
 var cookieValue = 'on';                     // Value of cookie
  
 function createDiv(){
-    var bodytag = document.getElementsByTagName('body')[0];
-    var div = document.createElement('div');
-    div.setAttribute('id','cookie-law');
-    div.setAttribute('class', 'panel panel-default')
-    div.innerHTML = '<p id="cookie-message" class="panel-body">Our website uses cookies to improve your experience. <a href="/about-us/privacy-policy/" rel="nofollow" title="Privacy Policy">Find out more</a>. <a class="close-cookie-banner pull-right badge" href="javascript:void(0);" onclick="removeMe();"><span>X</span></a></p>';    
+  var bodytag = document.getElementsByTagName('body')[0];
+  var div = document.createElement('div');
+  div.setAttribute('id','cookie-law');
+  div.setAttribute('class', 'panel panel-default')
+  div.innerHTML = '<p id="cookie-message" class="panel-body">Our website uses cookies to improve your experience. <a href="/about-us/privacy-policy/" rel="nofollow" title="Privacy Policy">Find out more</a>. <a class="close-cookie-banner pull-right badge" href="javascript:void(0);" onclick="removeMe();"><span>X</span></a></p>';    
  // Be advised the Close Banner 'X' link requires jQuery
-     
-    // bodytag.appendChild(div); // Adds the Cookie Law Banner just before the closing </body> tag
-    // or
-    bodytag.insertBefore(div,bodytag.firstChild); // Adds the Cookie Law Banner just after the opening <body> tag
-     
-    document.getElementsByTagName('body')[0].className+=' cookiebanner'; //Adds a class tothe <body> tag when the banner is visible
-     
-    createCookie(window.cookieName,window.cookieValue, window.cookieDuration); // Create the cookie
+   
+  // bodytag.appendChild(div); // Adds the Cookie Law Banner just before the closing </body> tag
+  // or
+  bodytag.insertBefore(div,bodytag.firstChild); // Adds the Cookie Law Banner just after the opening <body> tag
+   
+  document.getElementsByTagName('body')[0].className+=' cookiebanner'; //Adds a class tothe <body> tag when the banner is visible
+   
+  createCookie(window.cookieName,window.cookieValue, window.cookieDuration); // Create the cookie
 }
  
  
 function createCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000)); 
-        var expires = "; expires="+date.toGMTString(); 
-    }
-    else var expires = "";
-    if(window.dropCookie) { 
-        document.cookie = name+"="+value+expires+"; path=/"; 
-    }
+  if (days) {
+	var date = new Date();
+	date.setTime(date.getTime()+(days*24*60*60*1000)); 
+	var expires = "; expires="+date.toGMTString(); 
+  }
+  else var expires = "";
+  if(window.dropCookie) { 
+	document.cookie = name+"="+value+expires+"; path=/"; 
+  }
 }
  
 function checkCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+	var c = ca[i];
+	while (c.charAt(0)==' ') c = c.substring(1,c.length);
+	if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
 }
  
 function eraseCookie(name) {
-    createCookie(name,"",-1);
+  createCookie(name,"",-1);
 }
  
 window.onload = function(){
-    if(checkCookie(window.cookieName) != window.cookieValue){
-        createDiv(); 
-    }
+  if(checkCookie(window.cookieName) != window.cookieValue){
+	createDiv(); 
+  }
 }
  
 function removeMe(){
