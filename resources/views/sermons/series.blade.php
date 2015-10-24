22ca161a7921c3306ce97d36024347028c37c605
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-10-24 18:45:45 +0100

    Added year to URL and paginated preacher and series pages

--- resources/views/sermons/series.blade.php
+++ resources/views/sermons/series.blade.php
@@ -3,19 +3,21 @@
 @section('dynamic_content')
 
 @foreach ($sermons as $sermon)
-      <h3><a href="/sermons/{{$sermon->slug}}">{{$sermon->title}}</a></h3> 
-      <p>
-        <span class="glyphicon glyphicon-calendar"></span>
-        &nbsp; {{date ('jS \of F', strtotime($sermon->date))}}
-      </p>
-      <p>
-        <span class="glyphicon glyphicon-user"></span> &nbsp
-        {{ $sermon->preacher }}
-      </p>
-      <p>
-        <span class="glyphicon glyphicon-book"></span> &nbsp
-        {{ $sermon->reference }}
-      </p>
-    @endforeach
+  <h3><a href="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">{{$sermon->title}}</a></h3> 
+  <p>
+    <span class="glyphicon glyphicon-calendar"></span>
+    &nbsp; {{date ('jS \of F', strtotime($sermon->date))}}
+  </p>
+  <p>
+    <span class="glyphicon glyphicon-user"></span> &nbsp
+    {{ $sermon->preacher }}
+  </p>
+  <p>
+    <span class="glyphicon glyphicon-book"></span> &nbsp
+    {{ $sermon->reference }}
+  </p>
+@endforeach
+
+{!! $sermons->render() !!}
 
 @stop
