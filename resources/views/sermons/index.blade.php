c34e249dc35f64a2843e842b1449902064288a87
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-10-24 19:15:27 +0100

    Tidied up series

--- resources/views/sermons/index.blade.php
+++ resources/views/sermons/index.blade.php
@@ -16,63 +16,13 @@
   <div class="col-md-6">
     <h2>Morning</h2>
     @foreach ($latest_morning_sermons as $sermon)
-      <h3><a href="sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">{{$sermon->title}}</a></h3> 
-      <p>
-        <span class="glyphicon glyphicon-calendar"></span>
-        &nbsp; {{date ('jS \of F Y', strtotime($sermon->date))}}
-      </p>
-      <p>
-        <span class="glyphicon glyphicon-user"></span> &nbsp
-        {{ $sermon->preacher }}
-      </p>
-      <p>
-        <span class="glyphicon glyphicon-book"></span> &nbsp
-        {{ $sermon->reference }}
-      </p>
-      @if ($user != null && $user->email == "admin@crockenhill.org")
-        {!! Form::open(array('action' => array('SermonController@destroy', $sermon->slug), 'method' => 'delete')) !!}
-          <div class="btn-group">
-            <a href="/sermons/{{$sermon->slug}}/edit" class="btn btn-primary">
-              Edit this sermon
-            </a>
-            
-            <button type="submit" class="btn btn-danger">
-              Delete this sermon
-            </button>
-          </div>
-        {!! Form::close() !!}
-      @endif
+      @include('includes.sermon-display')
     @endforeach
   </div>
   <div class="col-md-6">
     <h2>Evening</h2>
-        @foreach ($latest_evening_sermons as $sermon)
-      <h3><a href="sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">{{$sermon->title}}</a></h3> 
-      <p>
-        <span class="glyphicon glyphicon-calendar"></span>
-        &nbsp; {{date ('jS \of F Y', strtotime($sermon->date))}}
-      </p>
-      <p>
-        <span class="glyphicon glyphicon-user"></span> &nbsp
-        {{ $sermon->preacher }}
-      </p>
-      <p>
-        <span class="glyphicon glyphicon-book"></span> &nbsp
-        {{ $sermon->reference }}
-      </p>
-      @if ($user != null && $user->email == "admin@crockenhill.org")
-        {!! Form::open(array('action' => array('SermonController@destroy', $sermon->slug), 'method' => 'delete')) !!}
-          <div class="btn-group">
-            <a href="/sermons/{{$sermon->slug}}/edit" class="btn btn-primary">
-              Edit this sermon
-            </a>
-            
-            <button type="submit" class="btn btn-danger">
-              Delete this sermon
-            </button>
-          </div>
-        {!! Form::close() !!}
-      @endif
+    @foreach ($latest_evening_sermons as $sermon)
+      @include('includes.sermon-display')
     @endforeach
   </div>
 </div>
