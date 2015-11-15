2f88a7939f3e67115ee96ce5687cfc47806f15b4
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-11-15 22:20:57 +0000

    Sermon tests working

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,7 +53,7 @@
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<input type="submit" class="btn btn-primary" value="Login">Login</input>
+								<input type="submit" class="btn btn-primary" value="Login"></input>
 
 								<a class="btn btn-link" href="{{ url('/members/password/email') }}">Forgot Your Password?</a>
 							</div>
