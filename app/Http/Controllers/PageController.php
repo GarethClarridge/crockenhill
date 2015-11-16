a9e0cbe7762b618cb33d89ce64812e818c7d2841
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-11-16 21:27:40 +0000

    Got rid of subpage nonsense

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,20 +2,17 @@
 
 class PageController extends BaseController {
 
-  protected $layout = 'layouts.main';
-
-	public function showPage($slug)
+	public function showPage($area = 'members', $slug = NULL)
 	{
+		//Area defaults to members, slug defaults to null
+
+		if ($slug === NULL) {
+			$slug = $area;
+		}
+
     if ($page = \Crockenhill\Page::where('slug', $slug)->first()) {
-
-	    if (\Request::is('members/members-area')) {
-	    	$area = 'members';
-		    $breadcrumbs = '<li class="active">'.$page->heading.'</li>';
-	    } else {
-	    	$area = $slug;
-		    $breadcrumbs = '<li class="active">'.$page->heading.'</li>';
-	    }
-
+		  
+		  $breadcrumbs = '<li class="active">'.$page->heading.'</li>';
 	    $links = \Crockenhill\Page::where('area', $area)
 	    	->where('slug', '!=', $slug)
 	    	->where('slug', '!=', $area)
@@ -24,7 +21,6 @@ class PageController extends BaseController {
 	    	->orderBy(\DB::raw('RAND()'))
 	    	->take(5)
 	    	->get();
-	    
 	    $description = '<meta name="description" content="'.$page->description.'">';
 	    
 			return view('page', array(
@@ -35,49 +31,6 @@ class PageController extends BaseController {
 		    'breadcrumbs'   => $breadcrumbs,
 		    'content'       => htmlspecialchars_decode($page->body),
 		    'links'					=> $links,
-			));
-		} else {
-			\App::abort(404);
-		};
-	}
-
-	public function showSubPage($area, $slug)
-	{
-	    if ($page = \Crockenhill\Page::where('slug', $slug)->first()) {
-		    $parent = \Crockenhill\Page::where('slug', $area)->first();
-
-		    $breadcrumb = '<li>'.link_to($page['area'], $parent->heading).'&nbsp</li><li class="active">'.$page->heading.'</li>';
-		    $description = '<meta name="description" content="'.$page->description.'">';
-
-				return view('page', array(
-			    'slug'          => $page->slug,
-			    'heading'       => $page->heading,		    
-			    'description'   => $description,
-			    'area'					=> $page->area,
-			    'breadcrumbs'   => $breadcrumb,
-			    'content'       => htmlspecialchars_decode($page->body),
-				));
-			} else {
-				\App::abort(404);
-			};
-	}
-
-	public function showMemberHomepage()
-	{
-    if ($page = \Crockenhill\Page::where('slug', 'members')->first()) {
-	    
-    	$area = 'members';
-	    $breadcrumbs = '<li class="active">'.$page->heading.'</li>';
-	    
-	    $description = '<meta name="description" content="'.$page->description.'">';
-	    
-			return view('page', array(
-		    'slug'          => $page->slug,
-		    'heading'       => $page->heading,		    
-		    'description'   => $description,
-		    'area'					=> $page->area,
-		    'breadcrumbs'   => $breadcrumbs,
-		    'content'       => htmlspecialchars_decode($page->body)
 			));
 		} else {
 			\App::abort(404);
