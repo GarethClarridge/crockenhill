6dd14e2b9a36dc6b25c348dccbf139beb17f3708
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-11-28 22:27:24 +0000

    Removed references to laravel forms

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -4,44 +4,45 @@
 
   <form method="POST" action="/members/pages" accept-charset="UTF-8" enctype="multipart/form-data" class="create">
 
-    {{ Form::open(array('route' => 'members.pages.store', 'files' => true)) }}
+  {{ Form::open(array('route' => 'members.pages.store', 'files' => true)) }}
 
-        <div class="form-group">
-            {{ Form::label('heading', 'Heading') }}
-            {{ Form::text('heading', $value = null, array('class' => 'form-control')) }}
-        </div>
+    <div class="form-group">
+      <label for="heading">Heading</label>
+      <input class="form-control h1" id="heading" name="heading" type="text">
+    </div>
 
-            <div class="form-group">
-              {{ Form::label('image', 'Image') }}
-              {{ Form::file('image') }}
-            </div>
+    <div class="form-group">
+      <label for="image">Header image</label>
+      <input name="image" type="file" id="image">
+    </div>
 
-        <div class="form-group">
-            {{ Form::label('area', 'Website Area') }}
-            {{ Form::select('area', array(
-                'about-us'      => 'About Us', 
-                'whats-on'      => 'What\'s On', 
-                'find-us'       => 'Find Us', 
-                'contact-us'    => 'Contact Us',
-                'sermons'       => 'Sermons',
-                'members'       => 'Members'
-                ), $value = null, array('class' => 'form-control')) }}
-        </div>
+    <div class="form-group">
+      <label for="area">Website area</label>
+      <select class="form-control" id="area" name="area">
+        <option value="about-us">About us</option>
+        <option value="whats-on">What's on</option>
+        <option value="find-us">Find us</option>
+        <option value="contact-us">Contact us</option>
+        <option value="sermons">Sermons</option>
+        <option value="members">Members</option>
+      </select>
+    </div>
 
-        <div class="form-group">
-            {{ Form::label('description', 'Description') }}
-            {{ Form::text('description', $value = null, array('class' => 'form-control')) }}
-        </div>
+    <div class="form-group">
+      <label for="description">Page description</label>
+      <input class="form-control" name="description" type="text" id="description">
+    </div>
 
-        <div class="form-group">
-            {{ Form::label('body', 'Content') }}
-            {{ Form::textarea('body', $value = null, array('class' => 'form-control')) }}
-        </div>
-        <div class="form-actions">
-            {{ Form::submit('Save', array('class' => 'btn btn-success btn-save btn-large')) }}
-            <a href="{{ URL::route('members.pages.index') }}" class="btn btn-large">Cancel</a>
-        </div>
+    <div class="form-group">
+      <label for="body">Page content</label>
+      <input class="form-control" name="body" type="textarea" id="body">
+    </div>
 
-    {{ Form::close() }}
+    <div class="form-actions">
+      <input class="btn btn-success btn-save btn-large" type="submit" value="Save">
+      <a href="{!! URL::route('members.pages.index') !!}" class="btn btn-large">Cancel</a>
+    </div>
+
+  {{ Form::close() }}
  
 @stop
