c627ad632f345ee6d342b5efa35a6e3de90931a1
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2015-12-30 17:57:15 +0000

    Enabled page deletion

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -30,14 +30,13 @@
             <td>{{ $page->updated_at }}</td>
             <td>
               <a href="{{ URL::route('members.pages.edit', $page->slug) }}" class="btn btn-success">Edit</a>
-              <!-- <a href="{{ URL::route('members.pages.changeimage', $page->slug) }}" class="btn btn-success">Change Image</a>
               {!! Form::open(array('route' => array('members.pages.destroy', $page->slug), 
                           'method' => 'delete', 
                           'data-confirm' => 'Are you sure you want to delete this page?', 
                           'class' => 'form-inline')) !!}
               <button type="submit" href="{{ URL::route('members.pages.destroy', $page->slug) }}" class="btn btn-danger">
                 Delete
-              </button> -->
+              </button>
               {!! Form::close() !!}
             </td>
           </tr>
