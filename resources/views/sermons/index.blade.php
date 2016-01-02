530c8e48d1e77ec0da35eb5505426ad44b854aec
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2016-01-02 19:00:47 +0000

    Handles missing sermons as well as both sermons

--- resources/views/sermons/index.blade.php
+++ resources/views/sermons/index.blade.php
@@ -12,7 +12,7 @@
       <a href="/sermons/create" class="btn btn-primary btn-lg btn-block" role="button">Upload a new sermon</a>
   @endif
 
-  @foreach ($latest_sermons as $date => $sermon)
+  @foreach ($latest_sermons as $date => $sermons)
     <section class="week">
       <div class="row">
         <div class="col-md-2">
@@ -21,44 +21,81 @@
             <p class="panel-body text-center">{{ date('j', strtotime($date)) }}</p>
           </h2>
         </div>
-        @foreach ($sermon as $sermon)
-          <div class="col-md-5">
-            <h3>Morning</h3>
-            @if ($sermon->service === "morning")
-              <h4 class="h3">
-                <a href="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">{{$sermon->title}}</a>
-              </h4> 
-              <p>
-                <span class="glyphicon glyphicon-user"></span> &nbsp
-                <a href="/sermons/preachers/{{ \Illuminate\Support\Str::slug($sermon->preacher) }}">{{ $sermon->preacher }}</a>
-              </p>
-              <p>
-                <span class="glyphicon glyphicon-book"></span> &nbsp
-                {{ $sermon->reference }}
-              </p>
-              @if ($user != null && $user->email == "admin@crockenhill.org")
-                <form method="POST" action="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/delete" accept-charset="UTF-8">
-                  {!! Form::token() !!}
-                  <div class="btn-group">
-                    <a href="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/edit" class="btn btn-primary">
-                      Edit this sermon
-                    </a>
-                    
-                    <button type="submit" class="btn btn-danger">
-                      Delete this sermon
-                    </button>
-                  </div>
-                </form>
+        @if (count($sermons) != 2)
+          @foreach ($sermons as $sermon)
+            <div class="col-md-5">
+              <h3>Morning</h3>
+              @if ($sermon->service === "morning")
+                <h4 class="h3">
+                  <a href="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">{{$sermon->title}}</a>
+                </h4> 
+                <p>
+                  <span class="glyphicon glyphicon-user"></span> &nbsp
+                  <a href="/sermons/preachers/{{ \Illuminate\Support\Str::slug($sermon->preacher) }}">{{ $sermon->preacher }}</a>
+                </p>
+                <p>
+                  <span class="glyphicon glyphicon-book"></span> &nbsp
+                  {{ $sermon->reference }}
+                </p>
+                @if ($user != null && $user->email == "admin@crockenhill.org")
+                  <form method="POST" action="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/delete" accept-charset="UTF-8">
+                    {!! Form::token() !!}
+                    <div class="btn-group">
+                      <a href="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/edit" class="btn btn-primary">
+                        Edit this sermon
+                      </a>
+                      
+                      <button type="submit" class="btn btn-danger">
+                        Delete this sermon
+                      </button>
+                    </div>
+                  </form>
+                @endif
+              @else
+                <p>Sorry, no morning sermon is available for this week.</p>
+                <p>This is most likely due to a technical error.</p>
               @endif
-            @else
-              <p>Sorry, no morning sermon is available for this week.</p>
-              <p>This is most likely due to a technical error.</p>
-            @endif
-          </div>
+            </div>
 
-          <div class="col-md-5">
-            <h3>Evening</h3>
-            @if ($sermon->service === "evening")
+            <div class="col-md-5">
+              <h3>Evening</h3>
+              @if ($sermon->service === "evening")
+                <h4 class="h3">
+                  <a href="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">{{$sermon->title}}</a>
+                </h4> 
+                <p>
+                  <span class="glyphicon glyphicon-user"></span> &nbsp
+                  <a href="/sermons/preachers/{{ \Illuminate\Support\Str::slug($sermon->preacher) }}">{{ $sermon->preacher }}</a>
+                </p>
+                <p>
+                  <span class="glyphicon glyphicon-book"></span> &nbsp
+                  {{ $sermon->reference }}
+                </p>
+                @if ($user != null && $user->email == "admin@crockenhill.org")
+                  <form method="POST" action="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/delete" accept-charset="UTF-8">
+                    {!! Form::token() !!}
+                    <div class="btn-group">
+                      <a href="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/edit" class="btn btn-primary">
+                        Edit this sermon
+                      </a>
+                      
+                      <button type="submit" class="btn btn-danger">
+                        Delete this sermon
+                      </button>
+                    </div>
+                  </form>
+                @endif
+              @else
+                <p>Sorry, no evening sermon is available for this week.</p>
+                <p>We might have been at another church for a joint service,
+                    or there could have been a technical error.</p>
+              @endif
+            </div>
+          @endforeach
+        @else
+          @foreach ($sermons as $sermon)
+            <div class="col-md-5">
+              <h3>{{ \Illuminate\Support\Str::title($sermon->service)}}</h3>
               <h4 class="h3">
                 <a href="/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">{{$sermon->title}}</a>
               </h4> 
@@ -84,13 +121,9 @@
                   </div>
                 </form>
               @endif
-            @else
-              <p>Sorry, no evening sermon is available for this week.</p>
-              <p>We might have been at another church for a joint service,
-                  or there could have been a technical error.</p>
-            @endif
-          </div>
-        @endforeach
+            </div>            
+          @endforeach
+        @endif
         <br>
       </div>
     </section>
