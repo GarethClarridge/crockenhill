45fff0032b4013f4a6a96135614d6e0c9f2a76f4
Gareth Clarridge <garethclarridge@hotmail.co.uk>
2016-02-17 21:59:58 +0000

    Fixed errors

--- config/app.php
+++ config/app.php
@@ -77,7 +77,7 @@ return [
     |
     */
     'key' => env('APP_KEY', 'SomeRandomString'),
-    'cipher' => 'MCRYPT_RIJNDAEL_128',
+    'cipher' => 'AES-256-CBC',
     /*
     |--------------------------------------------------------------------------
     | Logging Configuration
@@ -136,6 +136,7 @@ return [
         Crockenhill\Providers\AuthServiceProvider::class,
         Crockenhill\Providers\EventServiceProvider::class,
         Crockenhill\Providers\RouteServiceProvider::class,
+        Crockenhill\Providers\ComposerServiceProvider::class,
     ],
     /*
     |--------------------------------------------------------------------------
