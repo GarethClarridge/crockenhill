13962c5dce72c4d63e4918deedcd7da11b14154b
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-07-21 13:59:39 +0100

    Footer columns fixed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,50 +1,56 @@
-<div class="row">
+<div class="container">
+  <div class="row">
 
-  <section class="col-md-12 col-lg-6">
-    <div class="row" id="sermons-footer">
-      <h4><a href="/sermons">&nbsp&nbsp&nbsp Latest Sermons</a></h4>
-      <div class="col-md-6">
-        @include('includes.sermon-display', ['sermon' => $morning])
+    <section class="col-sm-6">
+      <div class="row" id="sermons-footer">
+        <h4><a href="/sermons">&nbsp&nbsp&nbsp Latest Sermons</a></h4>
+        <div class="col-md-6">
+          @include('includes.sermon-display', ['sermon' => $morning])
+          <br>
+        </div>
+        <div class="col-md-6">
+          @include('includes.sermon-display', ['sermon' => $evening])
+        </div>
       </div>
-      <div class="col-md-6">
-        @include('includes.sermon-display', ['sermon' => $evening])
+    </section>
+
+    <section class="col-sm-6">
+      <div class="row">
+        <div class="col-md-6">
+          <h4><a href="/contact-us">Get in touch</a></h4>
+          <p><span class="glyphicon glyphicon-earphone"></span> &nbsp 01322 663995</p>
+          <p><span class="glyphicon glyphicon-envelope"></span> &nbsp <a href="mailto:pastor@crockenhill.org">pastor@crockenhill.org</a>
+          </p>
+          <p><i class="fa fa-facebook"></i> &nbsp&nbsp&nbsp <a href="https://www.facebook.com/pages/Crockenhill-Baptist-Church/487590057946905">Like us on Facebook</a>
+          </p>
+          <!--<p class="text-center"><i class="fa fa-twitter"></i> &nbsp&nbsp
+            <a href="http://www.twitter.com/crockenhill">Follow us on Twitter</a>
+          </p> -->
+        </div>
+
+        <div class="col-md-6">
+          <h4><a href="/find-us">Address</a></h4>
+          <address>
+            Crockenhill Baptist Church<br />
+            Eynsford Road<br />
+            Crockenhill<br />
+            Kent<br />
+            BR8 8JS<br />
+          </address>
+        </div>
       </div>
-    </div>
-  </section>
-
-  <section class="col-md-6 col-lg-3">
-    <h4><a href="/contact-us">Get in touch</a></h4>
-    <p><span class="glyphicon glyphicon-earphone"></span> &nbsp 01322 663995</p>
-    <p><span class="glyphicon glyphicon-envelope"></span> &nbsp <a href="mailto:pastor@crockenhill.org">pastor@crockenhill.org</a>
-    </p>
-    <p><i class="fa fa-facebook"></i> &nbsp&nbsp&nbsp <a href="https://www.facebook.com/pages/Crockenhill-Baptist-Church/487590057946905">Like us on Facebook</a>
-    </p>
-    <!--<p class="text-center"><i class="fa fa-twitter"></i> &nbsp&nbsp 
-      <a href="http://www.twitter.com/crockenhill">Follow us on Twitter</a>
-    </p> -->
-  </section>
-
-  <section class="col-md-6 col-lg-3">
-    <h4><a href="/find-us">Address</a></h4>
-    <address>
-      Crockenhill Baptist Church<br />
-      Eynsford Road<br />
-      Crockenhill<br />
-      Kent<br />
-      BR8 8JS<br />
-    </address>
-  </section>
-</div>
-
-<div class="row">
-    <section class="col-md-4 col-md-offset-4">
-        <a href="http://www.fiec.org.uk">
-            <img src="/images/fiec-affiliated-tagline-white.png" width="100%">
-        </a>
     </section>
-    
-    <section class="col-sm-12">
-        <p class="text-center copyright"><small>&copy; {{ date('Y') }} Crockenhill Baptist Church</small></p>
-    </section>
-</div>
+  </div>
 
+  <div class="row">
+      <section class="col-md-4 col-md-offset-4">
+          <a href="http://www.fiec.org.uk">
+              <img src="/images/fiec-affiliated-tagline-white.png" width="100%">
+          </a>
+      </section>
+
+      <section class="col-sm-12">
+          <p class="text-center copyright"><small>&copy; {{ date('Y') }} Crockenhill Baptist Church</small></p>
+      </section>
+  </div>
+</div>
