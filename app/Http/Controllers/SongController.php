6e9164c44cf03610a46290ce6d519a69599950e6
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-10-11 23:21:47 +0100

    Add frequency sort

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -30,75 +30,24 @@ class SongController extends Controller {
     // Load songs
     $songs =\Crockenhill\Song::all();
 
-    // Books of the Bible
-    $books = array(
-              'Gen'   => 'Genesis',
-              'Exod'  => 'Exodus',
-              'Lev'   => 'Leviticus',
-              'Num'   => 'Numbers',
-              'Deut'  => 'Deuteronomy',
-              'Josh'  => 'Joshua',
-              'Judg'  => 'Judges',
-              'Ruth'  => 'Ruth',
-              '1Sam'  => '1 Samuel',
-              '2Sam'  => '2 Samuel',
-              '1Kgs'  => '1 Kings',
-              '2Kgs'  => '2 Kings',
-              '1Chr'  => '1 Chronicles',
-              '2Chr'  => '2 Chronicles',
-              'Ezr'   => 'Ezra',
-              'Neh'   => 'Nehemiah',
-              'Est'   => 'Esther',
-              'Job'   => 'Job',
-              'Ps'    => 'Psalms',
-              'Prov'  => 'Proverbs',
-              'Eccl'  => 'Ecclesiastes',
-              'Song'  => 'Song of Songs',
-              'Isa'   => 'Isaiah',
-              'Jer'   => 'Jeremiah',
-              'Lam'   => 'Lamentations',
-              'Exek'  => 'Exekiel',
-              'Dan'   => 'Daniel',
-              'Hos'   => 'Hosea',
-              'Joel'  => 'Joel',
-              'Amos'  => 'Amos',
-              'Obed'  => 'Obediah',
-              'Jonah' => 'Jonah',
-              'Mic'   => 'Micah',
-              'Nah'   => 'Nahum',
-              'Hab'   => 'Habbukuk',
-              'Zeph'  => 'Zephaniah',
-              'Hag'   => 'Haggai',
-              'Zech'  => 'Zechariah',
-              'Mal'   => 'Malachi',
-              'Matt'  => 'Matthew',
-              'Mark'  => 'Mark',
-              'Luke'  => 'Luke',
-              'John'  => 'John',
-              'Acts'  => 'Acts',
-              'Rom'   => 'Romans',
-              '1Cor'  => '1 Corinthians',
-              '2Cor'  => '2 Corinthians',
-              'Gal'   => 'Galatians',
-              'Eph'   => 'Ephesians',
-              'Phil'  => 'Phillipians',
-              'Col'   => 'Colossians',
-              '1Thess'=> '1 Thessalonians',
-              '2Thess'=> '2 Thessalonians',
-              '1Tim'  => '1 Timothy',
-              '2Tim'  => '2 Timothy',
-              'Titus' => 'Titus',
-              'Phm'   => 'Philemon',
-              'Heb'   => 'Hebrews',
-              'Jas'   => 'James',
-              '1Pet'  => '1 Peter',
-              '2Pet'  => '2 Peter',
-              '1John' => '1 John',
-              '2John' => '2 John',
-              '3John' => '3 John',
-              'Jude'  => 'Jude',
-              'Rev'   => 'Revelation'
-              );
+    foreach ($songs as $song) {
+      $last_played_record = \Crockenhill\PlayDate::where('song_id', $song->id)
+                                ->orderBy('date', 'desc')
+                                ->first();
+      if ($last_played_record) {
+        $last_played = $last_played_record->date;
+      } else {
+        $last_played = NULL;
+      }
+      $song['last_played'] = $last_played;
+
+      // Information about how often we've sung it recently
+      $years = 2;
+      $frequency = \Crockenhill\PlayDate::where('song_id', $song->id)
+                                ->where('date', '>', date('Y-m-d', strtotime("-".$years." years")))
+                                ->count();
+      $song['frequency'] = $frequency;
+    }
 
     // Present page
     return view('songs.index', array(
@@ -108,8 +57,7 @@ class SongController extends Controller {
       'area'        => $area,
       'breadcrumbs' => $breadcrumbs,
       'content'     => $content,
-      'songs'       => $songs,
-      'books'       => $books
+      'songs'       => $songs->sortByDesc('frequency')
     ));
   }
 
