d96712b7691528cc2d094e7573db49b43891b421
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-10-11 21:40:08 +0100

    Installed list.js

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -20,7 +20,7 @@
  </div>
  <br>
 
- {!! Form::open(array('url' => '/members/songs/scripture-reference-search')) !!}
+ <!-- {!! Form::open(array('url' => '/members/songs/scripture-reference-search')) !!}
    <div class="form-group">
      <div class="row">
        <div class="col-sm-3">
@@ -59,44 +59,61 @@
        </div>
      </div>
    </div>
- {!! Form::close() !!}
+ {!! Form::close() !!} -->
  <br>
 
-  @foreach ($songs as $song)
-    <div class="media song">
-      <div class="media-left media-middle praise-icon">
-        @if ($song->praise_number)
-          <img class="media-object" src="/images/praise.png" alt="">
-          <span class="praise-number">{!! $song->praise_number !!}</span>
-        @else
-          <img class="media-object" src="/images/Primary.png" width="128px" height="128px" alt="">
-        @endif
-      </div>
+  <section id="song-list">
+    <div class="form-group" id="song-list-controls">
+      <label for="text-filter">Filter songs</label>
+      <input class="search form-control" id='text-filter' />
 
-      <div class="media-body media-middle song-body">
-        <h3 class="media-heading">
-          @if (is_null($song->alternative_title))
-            <a href="/members/songs/{!!$song->id!!}/{!! \Illuminate\Support\Str::slug($song->title)!!}">{!!$song->title!!}</a>
-          @elseif (!is_null($song->alternative_title))
-            <a href="/members/songs/{!!$song->id!!}/{!! \Illuminate\Support\Str::slug($song->title)!!}">{!!$song->title!!} - ({!!$song->alternative_title!!})</a>
-          @endif
-        </h3>
-        @if ($song->author)
-          <p>
-            <span class="glyphicon glyphicon-user"></span> &nbsp
-            {!! $song->author !!}
-          </p>
-        @endif
-
-        @if ($song->copyright)
-          <p>
-            <span class="glyphicon glyphicon-copyright-mark"></span> &nbsp
-            {!! $song->copyright !!}
-          </p>
-        @endif
-      </div>
+      <button class="sort btn btn-default" data-sort="song-title">
+        Sort by title
+      </button>
     </div>
+    <ul class="list">
 
-  @endforeach
+      @foreach ($songs as $song)
+        <li class="media song">
+          <div class="media-left media-middle praise-icon">
+            @if ($song->praise_number)
+              <img class="media-object" src="/images/praise.png" alt="">
+              <span class="praise-number">{!! $song->praise_number !!}</span>
+            @else
+              <img class="media-object" src="/images/Primary.png" width="128px" height="128px" alt="">
+            @endif
+          </div>
+
+          <div class="media-body media-middle song-body">
+            <h3 class="media-heading">
+                <a href="/members/songs/{!!$song->id!!}/{!! \Illuminate\Support\Str::slug($song->title)!!}" class="song-title">{{$song->title}}</a>
+            </h3>
+            @if ($song->author)
+              <p>
+                <span class="glyphicon glyphicon-user"></span> &nbsp
+                <span class="song-author">{!! $song->author !!}</span>
+              </p>
+            @endif
+
+            @if ($song->copyright)
+              <p>
+                <span class="glyphicon glyphicon-copyright-mark"></span> &nbsp
+                {!! $song->copyright !!}
+              </p>
+            @endif
+          </div>
+        </li>
+
+      @endforeach
+    </section>
+
+  <script src="/scripts/list.min.js"></script>
+  <script type="text/javascript">
+    var options = {
+      valueNames: [ 'song-title', 'song-author', 'praise-number' ]
+    };
+
+    var songList = new List('song-list', options);
+  </script>
 
 @stop
