406dcc52023e2b4eb07c87bceccf901ec424450e
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-10-15 22:28:18 +0100

    Category filter

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -23,13 +23,13 @@
   <section id="song-list">
     <div class="song-filters">
       <div class="form-group">
-        <label for="text-filter">Filter songs</label>
+        <label for="song-text-filter">Filter songs</label>
         <input  class="search form-control"
-                id='text-filter'
+                id='song-text-filter'
                 placeholder="Try typing a song title, Praise! number, category or author"/>
       </div>
 
-      <div class="form-group">
+      <div class="form-group" id="in-praise-filter">
         <label>
           <input type="radio" name="in-praise" value="yes" id="filter-praise"> Only in Praise!
         </label>
@@ -40,6 +40,35 @@
           <input type="radio" name="in-praise" value="no" id="filter-nip"> Only not in Praise!
         </label>
       </div>
+
+      <div class="form-group" id="category-filter">
+
+        <div class="major-category-filter-div">
+          <label for="major-category-filter">Category:</label>
+          <select name="major-category-filter" id="major-category-filter" class="form-control">
+            <option value="All">All</option>
+            <option value="Approaching God">Approaching God</option>
+            <option value="The Father">The Father</option>
+            <option value="The Son">The Son</option>
+            <option value="The Holy Spirit">The Holy Spirit</option>
+            <option value="The Bible">The Bible</option>
+            <option value="The church">The church</option>
+            <option value="The gospel">The gospel</option>
+            <option value="The Christian life">The Christian life</option>
+            <option value="Christ's lordship over all of life">Christ's lordship over all of life</option>
+            <option value="The future">The future</option>
+          </select>
+        </div>
+
+        <div id="minor-category-filter-div" style="display:none;">
+          <label for="minor-category-filter">Sub-category:</label>
+          <select class="form-control" name="minor-category-filter" id="minor-category-filter">
+            <option id="minor-category-filter-option" value="All">All</option>
+          </select>
+        </div>
+
+      </div>
+
       <div class="form-group">
         Sort by: &nbsp &nbsp
         <button class="sort btn btn-default" data-sort="song-title">
@@ -106,13 +135,13 @@
               We've never sung this song.
             @endif
 
-            @if ($song->major_category)
+            @if ($song->major_category != '')
               <p>
                 <span class="glyphicon glyphicon-tag"></span> &nbsp
-                <span class="song-major-category">{{ $song->major_category }}</span>
+                <span class="song_major_category">{{ $song->major_category }}</span>
                 &nbsp &nbsp <span class="glyphicon glyphicon-tag"></span> &nbsp
                 @if ($song->minor_category)
-                    <span class="song-minor-category">{{ $song->minor_category }}</span>
+                    <span class="song_minor_category">{{ $song->minor_category }}</span>
                 @endif
               </p>
             @endif
@@ -129,7 +158,7 @@
   <script src="/scripts/jquery.min.js"></script>
   <script type="text/javascript">
     var options = {
-      valueNames: [ 'song-title', 'song-author', 'praise_number', 'song-frequency', 'song-major-category', 'song-minor-category' ]
+      valueNames: [ 'song-title', 'song-author', 'praise_number', 'song-frequency', 'song_major_category', 'song_minor_category' ]
     };
 
     var songList = new List('song-list', options);
@@ -153,7 +182,129 @@
       $('#filter-praise').click();
     });
 
+    $( "#category-filter" ).click(function() {
+      $('#filter-both-praise-nip').prop('checked', 'checked');
+    });
 
+    $( "#in-praise-filter" ).click(function() {
+      $('#major-category-filter').val('All');
+      $("#minor-category-filter-div").hide();
+    });
+
+    var categories = {
+      "Approaching God"                     : [ "The eternal Trinity",
+                                                "Adoration and thanksgiving",
+                                                "Creator and sustainer",
+                                                "Morning and evening",
+                                                "The Lord’s Day",
+                                                "Beginning and ending of the year"
+                                              ],
+      "The Father"                          : [ "His character",
+                                                "His providence",
+                                                "His love",
+                                                "His covenant"
+                                              ],
+      "The Son"                             : [ "His name and praise",
+                                                "His birth and childhood",
+                                                "His life and ministry",
+                                                "His suffering and death",
+                                                "His resurrection",
+                                                "His ascension and reign",
+                                                "His priesthood and intercession",
+                                                "His return in glory"
+                                              ],
+      "The Holy Spirit"                     : [ "His person and power",
+                                                "His presence in the church",
+                                                "His work in revival"
+                                              ],
+      "The Bible"                           : [ "Authority and sufficiency",
+                                                "Enjoyment and obedience"
+                                              ],
+      "The church"                          : [ "Character and privileges",
+                                                "Fellowship",
+                                                "Gifts and ministries",
+                                                "The life of prayer",
+                                                "Evangelism and mission",
+                                                "Baptism",
+                                                "The Lord’s Supper"
+                                              ],
+      "The gospel"                          : [ "Invitation and warning",
+                                                "Crying out for God",
+                                                "New birth and new life",
+                                                "Repentance and faith"
+                                              ],
+      "The Christian life"                  : [ "Union with Christ",
+                                                "Love for Christ",
+                                                "Freedom in Christ",
+                                                "Submission and trust",
+                                                "Assurance and hope",
+                                                "Peace and joy",
+                                                "Holiness",
+                                                "Humbling and restoration",
+                                                "Commitment and obedience",
+                                                "Zeal in service",
+                                                "Guidance",
+                                                "Suffering and trial",
+                                                "Spiritual warfare",
+                                                "Perseverance",
+                                                "Facing death"
+                                              ],
+      "Christ's lordship over all of life"  : [ "The earth and harvest",
+                                                "Christian citizenship",
+                                                "Christian marriage",
+                                                "Families and children",
+                                                "Health and healing",
+                                                "Work and leisure",
+                                                "Those in need",
+                                                "Government and nations"
+                                              ],
+      "The future"                          : [ "The resurrection of the body",
+                                                "Judgement and hell",
+                                                "Heaven and glory"
+                                              ]
+    };
+
+    $("#major-category-filter").change(function () {
+        var selection = this.value;
+
+        if (selection === 'All'){
+          songList.filter();
+          $("#minor-category-filter-div").hide();
+        }
+        else {
+          songList.filter(function (item) {
+              return item.values().song_major_category === selection;
+          });
+
+          var seloption = '<option id="minor-category-filter-option" value="All">All</option>';
+          var optionsarray = categories[selection];
+
+          $.each(optionsarray,function(i){
+              seloption += '<option class="generated-option" value="'+optionsarray[i]+'">'+optionsarray[i]+'</option>';
+          });
+
+          $(".generated-option").remove();
+          $("#minor-category-filter-option").replaceWith(seloption);
+
+          $("#minor-category-filter-div").show();
+        }
+    });
+
+    $("#minor-category-filter").change(function () {
+        var selection = this.value;
+
+        if (selection === 'All') {
+          var song_major_category = $("#major-category-filter").val();
+          console.log(song_major_category);
+          songList.filter(function (item) {
+            return item.values().song_major_category === song_major_category;
+          });
+        } else {
+          songList.filter(function (item) {
+              return item.values().song_minor_category === selection;
+          });
+        }
+    });
   </script>
 
 @stop
