81d096912af732605af6d157237a8467ecc08514
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-12-06 21:17:55 +0000

    Improved message for songs not in the current list

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -155,7 +155,7 @@
             @if ($song->recommended === 0)
             <p>
               <span class="glyphicon glyphicon-warning-sign"></span> &nbsp
-              Mark doesn't recommend choosing this song.
+              This song isn't in our current list of songs to sing. Is there an alternative?
             </p>
             @endif
 
