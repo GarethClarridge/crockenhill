858031d520cae2c026fcffbf0ac45c99c72a6779
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-12-29 23:33:36 +0000

    Application service providers

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,12 +17,11 @@ class RouteServiceProvider extends ServiceProvider {
 	/**
 	 * Define your route model bindings, pattern filters, etc.
 	 *
-	 * @param  \Illuminate\Routing\Router  $router
 	 * @return void
 	 */
-	public function boot(Router $router)
-	{		
-		parent::boot($router);
+	public function boot()
+	{
+		parent::boot();
 	}
 
 	/**
