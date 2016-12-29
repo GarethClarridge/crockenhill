7dabd7646c6f9877de80768caa34cdfa444d3381
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-12-29 23:49:01 +0000

    Broadcasting

--- app/Http/Controllers/Auth/ResetsPasswordController.php
+++ app/Http/Controllers/Auth/ResetsPasswordController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Crockenhill\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
@@ -26,7 +26,7 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
-    
+
     /**
      * Create a new controller instance.
      *
