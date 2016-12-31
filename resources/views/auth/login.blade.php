d312c771137dfcaff3be7ab98323750939df5fba
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-12-31 22:42:09 +0000

    Improved sign in error message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,12 +15,7 @@
 				<div>
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems:<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
+							You have entered an invalid username or password.
 						</div>
 					@endif
 
