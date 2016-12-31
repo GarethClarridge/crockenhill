1aedf3f451cf2a9bb3a2352bfd8f5baf7de26451
Gareth Clarridge <gareth.clarridge@outlook.com>
2016-12-31 22:52:21 +0000

    Hidden buttons behind @can statements

--- resources/views/members/home.blade.php
+++ resources/views/members/home.blade.php
@@ -2,18 +2,25 @@
 
 @section('dynamic_content')
 
-<p>Welcome to the members area of the website.
-  If you have any problems, talk to Gareth.
-  Your account will need to be set up to access some features.</p>
-<a href="/sermons" class="btn btn-default btn-lg btn-block">Edit sermons</a>
-<a href="/members/songs" class="btn btn-default btn-lg btn-block">View song list</a>
-<a href="/members/pages" class="btn btn-default btn-lg btn-block">Edit pages</a>
-<a href="/members/documents" class="btn btn-default btn-lg btn-block">View documents</a>
+  <p>Welcome to the members area of the website.
+    If you have any problems, talk to Gareth.
+  </p>
 
-<form action="logout" method="post">
-  <br>
-  <input type="hidden" name="_token" value="{{ csrf_token() }}">
- <button type="submit" name="logout" class="btn btn-primary btn-lg btn-block" role="button">Log out</button>
-</form>
+  @can ('edit-sermons')
+    <a href="/sermons" class="btn btn-default btn-lg btn-block">Edit sermons</a>
+  @endcan
+  <a href="/members/songs" class="btn btn-default btn-lg btn-block">View song list</a>
+  @can ('edit-pages')
+    <a href="/members/pages" class="btn btn-default btn-lg btn-block">Edit pages</a>
+  @endcan
+  @can ('edit-documents')
+    <a href="/members/documents" class="btn btn-default btn-lg btn-block">View documents</a>
+  @endcan
+
+  <form action="logout" method="post">
+    <br>
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+   <button type="submit" name="logout" class="btn btn-primary btn-lg btn-block" role="button">Log out</button>
+  </form>
 
 @stop
