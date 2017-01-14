c1bb59612d930499e62f7017c33451493f1d73ab
Gareth Clarridge <gareth.clarridge@outlook.com>
2017-01-14 23:04:55 +0000

    Added points to sermons table

---
+++ database/migrations/2017_01_14_230141_add_points_to_songs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPointsToSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     public function up()
+     {
+       Schema::table('sermons', function (Blueprint $table) {
+         $table->text('points')->nullable();
+       });
+     }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('sermons', function (Blueprint $table) {
+        $table->dropColumn('points');
+      });    }
+}
