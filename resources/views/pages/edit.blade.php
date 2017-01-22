c55b2c13acb21d04edb6a41667357d075f667217
Gareth Clarridge <gareth.clarridge@outlook.com>
2017-01-22 17:10:48 +0000

    Removed experiments, just copy text into textarea

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -92,6 +92,26 @@
               </div>
             </div>
 
+            <div class="photo-selector">
+              <div class="row">
+                <h2>Add a photo</h2>
+
+                <div class="col-sm-3 thumbnail text-center">
+                  <img class="img-responsive" src="/images/homepage/drinking.JPG" alt="">
+                  <div class="caption">
+                    <span>
+                      ![
+                      altText
+                      ](
+                      path
+                      )
+                      </span>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
             <div class="row">
               <div class="col-sm-6">
                 <div class="form-group">
@@ -109,7 +129,7 @@
                   Rendered content
                 </h4>
                 <div id="rendered-content">
-
+                  {!!$page->body!!}
                 </div>
               </div>
             </div>
@@ -120,7 +140,6 @@
             </div>
 
           </form>
-
         </div>
       </article>
       <br><br>
@@ -130,14 +149,18 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.6.0/showdown.min.js
 " charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" charset="utf-8"></script>
 <script type="text/javascript">
 
   function markdownInit() {
-    document.getElementById("markdown-input").onblur = function markdown() {
-      console.log ("Click");
+    var markdown      = document.getElementById("markdown-input");
+    var render        = document.getElementById("rendered-content");
+    markdown.onfocus = function blankRender() {
+      render.innerHTML = '';
+    }
+    markdown.onblur = function markdown() {
       var converter     = new showdown.Converter();
-      var markdown      =document.getElementById("markdown-input");
-      var render        = document.getElementById("rendered-content");
+      var markdown      = document.getElementById("markdown-input");
       render.innerHTML  = converter.makeHtml(markdown.value);
     }
   }
