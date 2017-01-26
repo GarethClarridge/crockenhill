d3f206313c94fed6cdc5da54f72ee55af8e2fd14
Gareth Clarridge <gareth.clarridge@outlook.com>
2017-01-26 19:27:48 +0000

    Changed photo directory

---
+++ resources/views/includes/photoselector.blade.php
@@ -0,0 +1,26 @@
+<div class="photo-selector">
+  <div class="row">
+    <h2>Add a photo</h2>
+
+    @foreach ($photos as $photo)
+
+      <div class="col-sm-3 thumbnail text-center">
+        <img class="img-responsive" src="{{$photo_directory}}/{{$photo}}" alt="">
+        <div class="caption">
+          <span>
+            <span class="glyphicon glyphicon-duplicate"></span> &nbsp
+            Copy this into the textarea below:
+            <br>
+            ![
+            altText
+            ](
+            {{$photo_directory}}/{{$photo}}
+            )
+            </span>
+        </div>
+      </div>
+
+    @endforeach
+
+  </div>
+</div>
