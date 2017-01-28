c24eaedf17c5cb51d26e482d96e984fb4b094f75
Gareth Clarridge <gareth.clarridge@outlook.com>
2017-01-28 18:40:05 +0000

    Basic print css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,8 @@
     @yield('description')
 
     <link type="text/css" rel="stylesheet" href="/stylesheets/main.css?v=5.1.0">
+    <link type="text/css" rel="stylesheet" href="/stylesheets/print.css" media="print">
+
     <link href='http://fonts.googleapis.com/css?family=Lato:400,300italic|Oswald' rel='stylesheet' type='text/css'>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
