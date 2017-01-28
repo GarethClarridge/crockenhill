5ccd1e64812e0ad08815f0601e1610414091cbde
Gareth Clarridge <gareth.clarridge@outlook.com>
2017-01-28 20:04:13 +0000

    Removed stuff from member controller

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -4,19 +4,6 @@ class MemberController extends Controller {
 
 	public function home()
 	{
-		$slug = 'members';
-		$area = 'members';
-		$heading = 'Members';
-		$description = 'Part of the website for church members';
-		$breadcrumbs = '<li class="active">'.$heading.'</li>';
-		$content = '';
-		return view('members.home', array(
-			'slug'          => $slug,
-			'heading'       => $heading,
-			'description'   => $description,
-			'area'					=> $area,
-			'breadcrumbs'   => $breadcrumbs,
-			'content'       => $content,
-		));
+		return view('members.home');
   }
 }
