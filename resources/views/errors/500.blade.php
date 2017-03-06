22d5c034239025b56b5ec7c1ab7cb71792b25f07
Gareth Clarridge <gareth.clarridge@outlook.com>
2017-03-06 20:00:31 +0000

    Adding 500 error

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main')
+
+@section('title', 'Page not found')
+
+@section('description', '<meta name="description" content="Page not found">')
+
+@section('content')
+
+<span class="nav-no-notch fixed-top-float">&nbsp</span>
+
+  <main class="container">
+      <div class="row">
+            <div class="col-md-9">
+                <article class="card">
+                    <div class="header-container">
+
+                    <h1><span>Sorry!<span></h1>
+
+                    </div>
+
+                    <p>Sorry, something has gone wrong.</p>
+                    <br>
+
+                    <a class="btn btn-primary btn-lg btn-block" href="/">Go to the homepage</a>
+
+                </article>
+            </div>
+        </div>
+  </main>
+@stop
