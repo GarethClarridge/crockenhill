87701cc9612c19009e44bdf1ed329e9a252bb4b1
Gareth Clarridge <gareth.clarridge@outlook.com>
2018-03-17 23:09:46 +0000

    ExceptionHandler fixed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,67 +1,53 @@
-<?php namespace Crockenhill\Exceptions;
+<?php
+
+namespace Crockenhill\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-class Handler extends ExceptionHandler {
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
 
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
-	protected $dontReport = [
-		'Symfony\Component\HttpKernel\Exception\HttpException'
-	];
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $e
-	 * @return void
-	 */
-	public function report(Exception $e)
-	{
-		return parent::report($e);
-	}
-
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $e
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $e)
-	{
-		if (config('app.debug') == true) {
-			return parent::render($request, $e);
-		} else {
-			// custom error message
-	    if ($e instanceof \ErrorException) {
-	      return response()->view('errors.500', [], 500);
-	    } else {
-	      return parent::render($request, $e);
-	    }
-		}
-	}
-
-	/**
-	 * Convert an authentication exception into an unauthenticated response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Illuminate\Auth\AuthenticationException  $exception
-	 * @return \Illuminate\Http\Response
-	 */
-	protected function unauthenticated($request, AuthenticationException $exception)
-	{
-	    if ($request->expectsJson()) {
-	        return response()->json(['error' => 'Unauthenticated.'], 401);
-	    }
-
-	    return redirect()->guest('login');
-	}
-
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
+		
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        return parent::render($request, $exception);
+    }
 }
