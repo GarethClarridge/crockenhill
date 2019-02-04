92c90b08c2c41f72e4f032c1f94dc8f760aadb6a
Gareth Clarridge <gareth.clarridge@outlook.com>
2019-02-04 20:40:30 +0000

    Page edit page

---
+++ resources/views/includes/photo-selector.blade.php
@@ -0,0 +1,30 @@
+<div class="photo-selector">
+  <div class="row">
+    <div class="col-12">
+      <h2 class="mb-5 text-white">Add a photo</h2>
+    </div>
+
+    @foreach ($photos as $photo)
+
+      <div class="card-group">
+        <div class="card">
+          <img class="card-img-top" src="{{$photo_directory}}/{{$photo}}" alt="">
+          <div class="card-body">
+            <p class="card-text">
+              <i class="far fa-copy"></i> &nbsp
+              Copy this into the markdown content box:
+              <br>
+              ![
+              altText
+              ](
+              {{$photo_directory}}/{{$photo}}
+              )
+            </p>
+          </div>
+        </div>
+      </div>
+
+    @endforeach
+
+  </div>
+</div>
--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -4,121 +4,111 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
-    <br><br><br>
-      <article class="card">
-        <form class="" action="/members/pages/{{$page->slug}}" method="post">
-          <input type="hidden" name="_method" value="PUT">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
-          <div class="header-container">
-            <h1>
-              <span>
-                <i class="fas fa-pencil-alt"></i> &nbsp &nbsp
-                <input class="edit-heading" id="heading" name="heading" type="text" value="{{$page->heading}}">
-              </span>
-            </h1>
+      <article class="card mt-3">
+        <div class="card-body">
+          <h2 class="card-title">Edit '{{$page->heading}}' page</h2>
+          <form class="" action="/members/pages/{{$page->slug}}" method="post">
+            <input type="hidden" name="_method" value="PUT">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+            <div>
+              @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                  <strong>Whoops!</strong> There were some problems:<br><br>
+                  <ul>
+                    @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                    @endforeach
+                  </ul>
+                </div>
+              @endif
+
+              <div class="edit-metadata mt-3">
+                <div class="form-group">
+                  <label for="heading">Heading</label>
+                  <input class="form-control" id="heading" name="heading" type="text" value="{{$page->heading}}">
+                </div>
+
+                <div class="form-group">
+                  <label for="description">Description <small>(returned on Google searches)</small></label>
+                  <input class="form-control" id="description" name="description" type="text" value="{{$page->description}}">
+                </div>
+
+                <div class="form-group">
+                  <label for="area">Website section</label>
+                  <select class="form-control" name="area" value="{{$page->area}}">
+                    @if ($page->area == 'about-us')
+                      <option value="about-us" selected>About us</option>
+                    @else
+                      <option value="about-us">About us</option>
+                    @endif
+
+                    @if ($page->area == 'whats-on')
+                      <option value="whats-on" selected>What's on</option>
+                    @else
+                      <option value="whats-on">What's on</option>
+                    @endif
+
+                    @if ($page->area == 'find-us')
+                      <option value="find-us" selected>Find us</option>
+                    @else
+                      <option value="find-us">Find us</option>
+                    @endif
+
+                    @if ($page->area == 'contact-us')
+                      <option value="contact-us" selected>Contact us</option>
+                    @else
+                      <option value="contact-us">Contact us</option>
+                    @endif
+
+                    @if ($page->area == 'sermons')
+                      <option value="sermons" selected>Sermons</option>
+                    @else
+                      <option value="sermons">Sermons</option>
+                    @endif
+
+                    @if ($page->area == 'members')
+                      <option value="members" selected>Members</option>
+                    @else
+                      <option value="members">Members</option>
+                    @endif
+                  </select>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-6">
+                  <div class="form-group">
+                    <label for="markdown" class="h4">Markdown content</label>
+                    @if ($page->markdown)
+                      <textarea class="form-control" name="markdown" id="markdown-input" rows="20">{{trim($page->markdown)}}</textarea>
+                    @else
+                      <textarea class="form-control" name="markdown" id="markdown-input" rows="20">{{trim($page->body)}}</textarea>
+                    @endif
+                  </div>
+                </div>
+
+                <div class="col-6">
+                  <h4>
+                    Rendered content
+                  </h4>
+                  <div id="rendered-content">
+                    {!!$page->body!!}
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-actions my-3">
+                <input class="btn btn-success btn-lg btn-block" type="submit" value="Save">
+                <div class="text-center">
+                  <a href="/members/pages/" class="btn btn-outline btn-large">Cancel</a>
+                </div>
+              </div>
+
+              @include('includes.photo-selector')
+
+            </form>
           </div>
-          <div>
-
-            @if (count($errors) > 0)
-              <div class="alert alert-danger">
-                <strong>Whoops!</strong> There were some problems:<br><br>
-                <ul>
-                  @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                  @endforeach
-                </ul>
-              </div>
-            @endif
-
-            <div class="edit-metadata">
-              <div class="row">
-                <div class="form-group">
-                  <div class="col-sm-2">
-                    <label for="description">Description</label>
-                  </div>
-                  <div class="col-sm-10">
-                    <input class="form-control" id="description" name="description" type="text" value="{{$page->description}}">
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="form-group">
-                  <div class="col-sm-2">
-                    <label for="area">Section</label>
-                  </div>
-                  <div class="col-sm-10">
-                    <select class="form-control" name="area" value="{{$page->area}}">
-                      @if ($page->area == 'about-us')
-                        <option value="about-us" selected>About us</option>
-                      @else
-                        <option value="about-us">About us</option>
-                      @endif
-
-                      @if ($page->area == 'whats-on')
-                        <option value="whats-on" selected>What's on</option>
-                      @else
-                        <option value="whats-on">What's on</option>
-                      @endif
-
-                      @if ($page->area == 'find-us')
-                        <option value="find-us" selected>Find us</option>
-                      @else
-                        <option value="find-us">Find us</option>
-                      @endif
-
-                      @if ($page->area == 'contact-us')
-                        <option value="contact-us" selected>Contact us</option>
-                      @else
-                        <option value="contact-us">Contact us</option>
-                      @endif
-
-                      @if ($page->area == 'sermons')
-                        <option value="sermons" selected>Sermons</option>
-                      @else
-                        <option value="sermons">Sermons</option>
-                      @endif
-
-                      @if ($page->area == 'members')
-                        <option value="members" selected>Members</option>
-                      @else
-                        <option value="members">Members</option>
-                      @endif
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            @include('includes.photoselector')
-
-            <div class="row">
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <label for="markdown">Markdown content</label>
-                  @if ($page->markdown)
-                    <textarea class="form-control" name="markdown" id="markdown-input" rows="20">{{trim($page->markdown)}}</textarea>
-                  @else
-                    <textarea class="form-control" name="markdown" id="markdown-input" rows="20">{{trim($page->body)}}</textarea>
-                  @endif
-                </div>
-              </div>
-
-              <div class="col-sm-6">
-                <h4>
-                  Rendered content
-                </h4>
-                <div id="rendered-content">
-                  {!!$page->body!!}
-                </div>
-              </div>
-            </div>
-
-            <div class="form-actions">
-              <input class="btn btn-success" type="submit" value="Save">
-              <a href="/members/pages/" class="btn btn-large">Cancel</a>
-            </div>
-
-          </form>
         </div>
       </article>
       <br><br>
