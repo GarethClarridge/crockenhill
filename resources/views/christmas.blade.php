b7491d1f07a7e61e65c4cda5f008b7a79371c723
Gareth Clarridge <gareth.clarridge@outlook.com>
2019-12-17 22:52:36 +0000

    Christmas 2019

--- resources/views/christmas.blade.php
+++ resources/views/christmas.blade.php
@@ -1,70 +1,83 @@
 @extends('layouts.main')
 
-@section('title', 'Crockenhill Baptist Church')
+@section('title')
+  Christmas
+@stop
 
-@section('description', '<meta name="description" content="We are an independent evangelical church located in the village of Crockenhill in Kent.">')
+@section('description')
+  Christmas at Crockenhill Baptist Church
+@stop
 
 @section('content')
-
   <main id="christmas-page">
-    <div class="christmas-head">
+    <div class="christmas-head text-white">
       <div class="container">
-        <h1>Christmas at Crockenhill Baptist Church</h1>
+        <h1 class="text-center">
+          Christmas at Crockenhill Baptist Church
+        </h1>
       </div>
     </div>
 
-    <section class="white-background">
-      <h2>
-        Adventurers' presentation
-      </h2>
-      <p>
-        A Christmas presentation by
-        <a href="/whats-on/adventurers">Adventurers</a>,
-        our group for 6-9 year olds.
-        Everyone is welcome, but families of children in Adventurers are especially invited.
-      </p>
-      <h3>
-        Sunday 11 December, 10:30-11:30
-      </h3>
+    <section class="white-background pt-5">
+      <div class="container">
+        <h2 class="text-center">
+          Adventurers' presentation
+        </h2>
+        <p class="text-center">
+          A Christmas presentation by
+          <a href="/whats-on/adventurers">Adventurers</a>,
+          our group for 6-9 year olds.
+          Everyone is welcome, but families of children in Adventurers are especially invited.
+        </p>
+        <h3 class="text-center">
+          Sunday 15 December, 10:30-11:30
+        </h3>
+      </div>
     </section>
 
-    <section class="white-background">
-      <h2>
-        Carols at the Chequers
-      </h2>
-      <p>
-        An evening of Christmas carol singing in the village pub,
-        at the kind invitation of the landlord.
-      </p>
-      <h3>
-        Wednesday 14 December, from 7:30pm
-      </h3>
+    <section class="white-background pt-5">
+      <div class="container">
+        <h2 class="text-center">
+          Carols at the Chequers
+        </h2>
+        <p class="text-center">
+          An evening of Christmas carol singing in the village pub,
+          at the kind invitation of the landlord.
+        </p>
+        <h3 class="text-center">
+          Wednesday 18 December, from 7:30pm
+        </h3>
+      </div>
     </section>
 
-    <section class="white-background">
-      <h2>
-        Carols by Candlelight
-      </h2>
-      <p>
-        A candlelit service of traditional Christmas carols and readings,
-        followed by refreshments.
-      </p>
-      <h3>
-        Sunday 18 December, 6:00-7:00pm
-      </h3>
+    <section class="white-background pt-5">
+      <div class="container">
+        <h2 class="text-center">
+          Carols by Candlelight
+        </h2>
+        <p class="text-center">
+          A candlelit service of traditional Christmas carols and readings,
+          followed by refreshments.
+        </p>
+        <h3 class="text-center">
+          Sunday 22 December, 6:00-7:00pm
+        </h3>
+      </div>
     </section>
 
-    <section class="white-background">
-      <h2>
-        Christmas Day Celebration
-      </h2>
-      <p>
-        We meet together on Christmas day to joyfully celebrate the birth of our Saviour - the almighty God born as a baby in Bethlehem.
-        Join us as we give glory to the new-born King.
-      </p>
-      <h3>
-        Sunday 25 December, 10:30-11:15am
-      </h3>
+    <section class="white-background pt-5">
+      <div class="container">
+        <h2 class="text-center">
+          Christmas Day Celebration
+        </h2>
+        <p class="text-center">
+          We meet together on Christmas day to joyfully celebrate the birth of our Saviour - the almighty God born as a baby in Bethlehem.
+          Join us as we give glory to the new-born King.
+        </p>
+        <h3 class="text-center">
+          Wednesday 25 December, 10:30-11:15am
+        </h3>
+      </div>
     </section>
 
   </main>
