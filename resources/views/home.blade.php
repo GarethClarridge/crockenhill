3e3ee264a1cdd96c9140c029d2ab95dce9647a53
Gareth Clarridge <gareth.clarridge@outlook.com>
2020-03-28 22:37:38 +0000

    Added resources back

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
     <!-- <div class="background-image" id="christmas">
       <div class="container">
         <div class="row justify-content-md-center">
-          <section class="col-md-10 col-lg-6 text-white p-5">
+          <section class="col-md-10 col-lg-8 text-white p-5">
               <p>
                 Come and celebrate the birth of our Saviour with us this Christmas.
               </p>
@@ -32,27 +32,46 @@
       </div>
     </div> -->
 
-    <section class="white-background pt-5">
+    <div class="pt-5" id="online">
       <div class="container">
-        <p class="text-center">
-          Sadly we can't meet together as a church in person at the moment,
-          <br>
-          but please join us on Sundays online instead.
-        </p>
-        <h2 class="text-center">
-          <a href="/online" class="btn btn-primary btn-lg" id="online-service-btn">Online services</a>
-        </h2>
-        <p class="text-center">
-          We plan to have an online service every week, with prayers, Bible readings,
-          <br>songs and Mark preaching the good news about Jesus to us from the Bible.
-        </p>
+        <div class="row justify-content-md-center">
+          <section class="col-md-10 col-lg-8 p-5">
+            <div class="white-background">
+              <p class="text-center">
+                Sadly we can't meet together as a church in person at the moment,
+                but please join us on Sundays online instead.
+              </p>
+              <p class="text-center">
+                <a href="/online" class="btn btn-primary btn-lg" id="online-service-btn">Online services</a>
+              </p>
+              <p class="text-center mb-3">
+                We plan to have an online service every week, with prayers, Bible readings,
+                songs and Mark preaching the good news about Jesus to us from the Bible.
+              </p>
+              <br>
+              <hr>
+              <br>
+              <p>
+                We want to keep growing our faith in Christ during this unexpected
+                separation. We've prepared a list of things we can be talking
+                to God about in prayer, and a list of online resources to help us
+                learn more about him.
+              </p>
+              <p class="mt-3">
+                <small>
+                  <a href="/resources" class="btn btn-secondary" id="online-service-btn">Resources for isolation</a>
+                </small>
+              </p>
+            </div>
+          </section>
+        </div>
       </div>
-    </section>
+    </div>
 
     <div class="background-image" id="listening">
       <div class="container">
         <div class="row justify-content-md-center">
-          <section class="col-md-10 col-lg-6 text-white p-5">
+          <section class="col-md-10 col-lg-8 text-white p-5">
               <p class="text-center">
                 Our church exists to worship God,
                 strengthen believers in their faith,
@@ -71,12 +90,14 @@
 
     <div class="container">
       <div class="row justify-content-md-center">
-        <section class="col-md-10 col-lg-6 p-5">
+        <section class="col-md-10 col-lg-8 p-5">
           <div class="white-background">
             <p class="text-center">
-              There are many activities at Crockenhill Baptist Church.
-              We have groups during term time for children and young people,
-              bible study and prayer groups and more.
+              During more normal times we run lots of activities to let as many
+              people as possible know the good news about Jesus.
+              We have Bible study and prayer groups meeting in people's homes,
+              and during term time groups for everyone from babies, children
+              and young people through to the more mature among us!
             </p>
             <br>
             <p class="text-center">
@@ -90,7 +111,7 @@
     <div class="background-image" id="building">
       <div class="container">
         <div class="row justify-content-md-center">
-          <section class="col-md-10 col-lg-6 text-white p-5">
+          <section class="col-md-10 col-lg-8 text-white p-5">
               <p class="text-center">
                 We are located in the village of Crockenhill in Kent,
                 which is a mile or so south of Swanley
