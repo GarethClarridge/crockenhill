1c5da13594d2a8057119c5a3496bf99a789464c5
Gareth Clarridge <gareth.clarridge@outlook.com>
2020-04-11 22:48:46 +0100

    Added Easter Sunday service

--- resources/views/easter.blade.php
+++ resources/views/easter.blade.php
@@ -11,6 +11,33 @@
 @section('content')
   <main class="container mb-3">
     <div class="row justify-content-md-center">
+      <div class="col-12">
+        <article class="card mt-3">
+          <div class="card-img-caption d-flex align-items-center">
+            <h1 class="card-text text-white">
+              <div class="px-2 py-1">
+                Easter Sunday
+              </div>
+            </h1>
+            <img class="card-img-top cbc-card-img-top" src="/images/homepage/easter.jpg">
+          </div>
+
+          <div class="card-body">
+            <div class="main-content text-dark">
+              <p>Join us as we celebrate the resurrection of Jesus together in our homes.</p>
+              <div class="text-dark mb-3 text-center">
+                  <a class="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=2HVvwvaitGA&list=PLvRlbyvqJwk6FhDWs8p1EkFawe-Fcetlf" role="button"><i class="fab fa-youtube"></i>&nbsp Watch on YouTube</a>
+              </div>
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/PLvRlbyvqJwk6FhDWs8p1EkFawe-Fcetlf" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+
+    <div class="row justify-content-md-center">
       <div class="col-md-10 col-lg-9">
         <article class="card mt-3">
           <div class="card-img-caption d-flex align-items-center">
@@ -39,11 +66,11 @@
 
     <div class="row justify-content-md-center">
       <div class="col-md-10 col-lg-9">
-        <article class="card mt-3">
+        <article class="card mt-3" id="why">
           <div class="card-img-caption d-flex align-items-center">
             <h1 class="card-text text-white">
               <div class="px-2 py-1">
-                Easter Sunday
+                What is Easter all about?
               </div>
             </h1>
             <img class="card-img-top cbc-card-img-top" src="/images/homepage/easter.jpg">
@@ -51,12 +78,106 @@
 
           <div class="card-body">
             <div class="main-content">
-              <p>Our Easter Sunday service will be here soon.</p>
+              <p>Easter is the centrepoint of Christianity, the highlight of the story,
+                the highest peak of the tallest mountain.
+              </p>
+              <p>
+                But if you think about it, it's a pretty odd highlight.
+                God himself come down to earth, not as a king to reign in splendour,
+                but as an ordinary man to die in agony.
+              </p>
+              <p>
+                Why?
+              </p>
+              <p>
+                This wasn't just pointless cruelty. Yes, the Romans crucified thousands,
+                but Jesus was more than just another statistic in a sea of brutality.
+                See, Jesus wasn't merely a victim.
+                Jesus chose to die.
+              </p>
+              <p>
+                Why?
+              </p>
+              <p>
+                Maybe we're looking at it wrong.
+                <em>
+                  Jesus wasn't so much a victim of the cross,
+                  as the cross was a victim of Jesus.
+                </em>
+                On the cross Jesus defeated death itself,
+                releasing his people from its power and their fear.
+              </p>
+              <p>
+                How?
+              </p>
+              <p>
+                Because as Jesus died he died for us.
+                Maybe you've seen people talk about how coronavirus
+                is Nature's way of punishing humanity for ruining the Earth.
+                Deep down we know we've messed up.
+                We've ruined a beautiful world.
+                When we look at how we all behave, we know that not one of us is innocent.
+              </p>
+              <p>
+                Jesus knew we are more guilty than we imagine.
+                We haven't offended an impersonal Nature, we've offended the God who made it.
+                The God who poured his life and love into human beings,
+                who then abused and hurt one another.
+                We hurt those God loved. And God was angry.
+                His judgement was worse than coronavirus. God's judgement was death.
+              </p>
+              <p>
+                But.
+              </p>
+              <p>
+                But God loved us.
+              </p>
+              <blockquote class="blockquote">
+                <p class="mb-0">
+                  "For God so loved the world that he gave his one and only Son,
+                  that whoever believes in him shall not perish but have eternal life."
+                </p>
+                <footer class="blockquote-footer">
+                  <cite title="Source Title">John 3:16, NIV</cite>
+                </footer>
+              </blockquote>
+              <p>
+                Jesus died so we don't have to. He paid the penalty we earned.
+                When on Easter Sunday he rose from the grave he proved he'd defeated death.
+                With the penalty paid, there's nothing to fear.
+              </p>
+              <p>
+                Not coronavirus, not even death itself.
+              </p>
+              <p>
+                Jesus holds out his arms and invites us to join him.
+                Join him in his death, and in his resurrected life.
+                Join him in a life free from the fear of knowing we're not good enough,
+                of one day being found out and judged.
+              </p>
+              <p>
+                This Easter, you have a choice.
+                Lock yourself down in perfect isolation,
+                or open up your heart and join yourself to Jesus.
+                Trust that he died for you that you might live for him.
+              </p>
+              <p>
+                And if you do that, whatever is going on in the world outside your house,
+                you'll be able to sing: <em>"Because he lives I can face tomorrow."</em>
+              </p>
+              <div class="embed-responsive embed-responsive-16by9 mb-3">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/HrpJr2m99es" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
               <div class="text-dark mb-3 text-center">
+                <a class="btn btn-secondary btn-lg" href="https://eastermeanshope.com/" role="button">Find out more at EasterMeansHope.com</a>
+              </div>
             </div>
           </div>
         </article>
       </div>
     </div>
+
+
+
   </main>
 @stop
