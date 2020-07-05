20de5a2ceff6fe409adcc11ebc11a12b3acac18d
Gareth Clarridge <gareth.clarridge@outlook.com>
2020-07-05 08:07:30 +0100

    5 July

--- resources/views/online.blade.php
+++ resources/views/online.blade.php
@@ -13,8 +13,44 @@
     <div class="online-head text-white">
       <div class="container mt-3">
 
+        gpdUail4EZo
 
+        <div class="row justify-content-md-center">
+          <div class="col-12">
+            <article class="card mb-3">
+              <div class="card-img-caption d-flex align-items-center">
+                <h1 class="card-text text-white">
+                  <div class="px-2 py-1">
+                    Sunday 5 July
+                  </div>
+                </h1>
+                <img class="card-img-top cbc-card-img-top" src="/images/headings/large/default.jpg">
+              </div>
 
+              <div class="card-body text-center">
+                <div class="main-content text-dark">
+                  <div class="text-dark mb-3 text-center">
+                      <a class="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=gpdUail4EZo" role="button"><i class="fab fa-youtube"></i>&nbsp Watch on YouTube</a>
+                  </div>
+                  <div class="mb-3 embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/gpdUail4EZo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer text-muted">
+                <small>
+                  New International Version (NIV)
+                  <br>
+                  Holy Bible, New International Version®,
+                  NIV® Copyright ©1973, 1978, 1984, 2011 by
+                  <a href="www.biblica.com">Biblica, Inc.®</a>
+                  Used by permission.
+                  All rights reserved worldwide.
+                </small>
+              </div>
+            </article>
+          </div>
+        </div>
 
         <div class="row justify-content-md-center">
           <div class="col-12">
