e6854cad76b92060eeb7d624555a804b1c8823b3
Gareth Clarridge <gareth.clarridge@outlook.com>
2020-07-11 12:47:27 +0100

    reopening route

---
+++ resources/views/reopening.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.main')
+
+@section('title')
+  Reopening
+@stop
+
+@section('description')
+  Reopening on 12 July
+@stop
+
+@section('content')
+  <main id="online-page" class="mb-3">
+    <div class="online-head text-white">
+      <div class="container mt-3">
+
+        <div class="row justify-content-md-center">
+          <div class="col-9">
+            <article class="card mb-3">
+              <div class="card-img-caption d-flex align-items-center">
+                <h1 class="card-text text-white">
+                  <div class="px-2 py-1">
+                    Reopening the church building
+                  </div>
+                </h1>
+                <img class="card-img-top cbc-card-img-top" src="/images/headings/large/default.jpg">
+              </div>
+
+              <div class="card-body">
+                <div class="main-content text-dark">
+                  <p>
+                    The good news is that the Government is now allowing churches to open up their buildings for corporate worship.
+                    Therefore, the intention is to re-open the church for worship on Sundays at the usual time of 10:30 a.m.
+                    We realise that not everybody will be able to come out and we would discourage all those over 70,
+                    or at an increased risk of contracting Covid-19, from making the effort to do so.
+                  </p>
+                  <p>
+                    If you are not able to make it to the morning services on Sundays for the time being,
+                    please don’t worry. We will be live-streaming the services here and <a href="https://www.youtube.com/channel/UCtSUTtkZlALToswWQpWS2kA/">on our YouTube channel</a>.
+                    We also intend to deliver recordings of the service for those not online.
+                    However, it should be noted that those who listen to DVDs will have to wait until later in the day on Sunday to receive their recording.
+                  </p>
+                  <p>
+                    In order to meet together we are seeking to follow <a href="https://www.gov.uk/government/publications/covid-19-guidance-for-the-safe-use-of-places-of-worship-during-the-pandemic-from-4-july">Government guidelines for places of worship</a>.
+                    This will mean that things will not be as normal.
+                    Here are a number of things for you to take note of:
+                  </p>
+                  <ul>
+                    <li>The meeting will be kept to within an hour.</li>
+                    <li>Please take care to observe 2m social distancing before, during and after the service.</li>
+                    <li>Please enter through the main front door and exit through the rear side-door (unless you are unable to go up and down the steps), as a one-way-system will be introduced.</li>
+                    <li>If you are unable to observe the one-way-system because of the steps through to the rear exit, you will be asked to wait until everyone has left the building.  You will then be able to exit through the main entrance.</li>
+                    <li>You will be asked to use anti-bacterial hand-wash as you enter and leave the building.</li>
+                    <li>You will be directed to a designated chair in the church upon entering the worship area by one of the stewards, and we would ask you to be amenable about this.  This is necessary to accommodate everyone within the limited amount of space available to us.  We realise people will be excited to see one another, but we must ask you to remain in your seat and not wander around the room before or after the service.</li>
+                    <li>Relevant notices will be put on the screen and emphasised at the start of the service. There will not be any notice sheets.</li>
+                    <li>We are not allowed to sing with our voices (only with our hearts!). However, there will be a couple of songs played over the P.A. system with the words projected onto the screen.</li>
+                    <li>If you wish to use a Bible you will need to bring your own, as there won’t be any Bibles or hymn books for you to use in the church.</li>
+                    <li>We would encourage online giving.  However, where this is not an option cash will be collected in the boxes.  You can put your money in a box as you enter or leave the building, but the one way system should be observed.</li>
+                    <li>If possible, we would ask you to avoid using the toilets.  However, they will be open for people to use. Please use the spray disinfectant made available to sanitise the cistern handle and toilet area after use. Throwaway paper towels will be provided along with bins to dispense with them. Children should be accompanied by an adult to ensure procedures are followed.</li>
+                    <li>Unfortunately we are not able to provide a crèche or Outback for the children at this point in time.</li>
+                    <li>There will be no refreshments served after the meeting. Neither will we be observing the Lord’s Supper for the time being.</li>
+                    <li>It is not necessary to wear a mask, but you can wear one if you wish to do so.</li>
+                    <li>We will endeavour to open windows and doors to maximise ventilation. Therefore, you may wish to consider putting on something warm if it is cold.</li>
+                    <li>A risk assessment has been drawn up for people to read on the church’s website.</li>
+                    <li>We are obliged to keep a record of those attending services for a period of time and you may be asked upon entering to provide your details if the church doesn’t already have them.</li>
+                    <li>Unfortunately we are not allowed to provide transport to those who may require it. If you are intent on coming but need transport, you will need to ask someone within your social bubble.</li>
+                  </ul>
+                  <p>
+                    This may all sound a little overwhelming, even draconian, but we hope that such measures will put everyone at ease so that we can focus upon the Lord and His goodness to us.
+                  </p>
+                  <p>Please let Laurie Everest (<a href="mailto:laurie.everest@btinternet.com">laurie.everest@btinternet.com</a>, 01322 664165) or Peter Clarridge (<a href="mailto:pclarridge@gmail.com">pclarridge@gmail.com</a>, 01322 404835) know if you are intending to come on Sundays by 1:00 p.m. on Saturdays.
+                    Ideally this should be done by email.  This will help us to make sure that we can safely accommodate everyone in the building.
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </main>
+@stop
