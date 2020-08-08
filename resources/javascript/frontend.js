111a696308e6c666138ca588b4f4ad868b5f21cd
Gareth Clarridge <gareth.clarridge@outlook.com>
2020-08-08 20:25:43 +0100

    Replaced sermons in footer with last weeks service from YouTube

--- resources/javascript/frontend.js
+++ resources/javascript/frontend.js
@@ -7,6 +7,15 @@ $(function() {
 		}
 	});
 
+	// Embed latest YouTube video
+	var channelID = "UCtSUTtkZlALToswWQpWS2kA";
+	var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+	$.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
+	   var link = data.items[1].link;
+	   var id = link.substr(link.indexOf("=")+1);
+	$("#last_week_youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
+	});
+
 });
 //
 // // Creare's 'Implied Consent' EU Cookie Law Banner v:2.4
