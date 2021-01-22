d6dcd7b20e45f5ab1807efeab96b30b0db4cfa63
Gareth Clarridge <gareth.clarridge@outlook.com>
2021-01-22 22:22:07 +0000

    switched videos

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -38,7 +38,7 @@
   </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="/scripts/bootstrap.min.js"></script>
-  <script src="/scripts/all.js?v=6.0.8"></script>
+  <script src="/scripts/all.js?v=6.0.9"></script>
   @stack('scripts')
 </head>
 
