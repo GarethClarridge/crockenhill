7030cbc9fb12b3b2386533bae7a28ad871e5ee88
Gareth Clarridge <gareth.clarridge@outlook.com>
2021-03-10 22:05:13 +0000

    Finished Christianity Explored page

---
+++ resources/views/full-width-pages/christianity-explored.blade.php
@@ -0,0 +1,182 @@
+@extends('layouts.main')
+
+@section('title')
+  Christmas
+@stop
+
+@section('description')
+  Christmas at Crockenhill Baptist Church
+@stop
+
+@section('content')
+<main>
+  <div id="christianity-explored">
+    <div class="christianity-explored-background pb-5">
+      <div class="container">
+        <img src="/images/christianityexplored.png" alt="Christianity Explored: what's the best news you've ever heard?" class="img-fluid">
+
+        <div class="row justify-content-md-center">
+          <div class="col-sm-10 col-md-8 col-lg-6">
+            <div class="text-center text-white my-5">
+              <h2>What is Christianity Explored?</h2>
+              <p>
+                Christianity Explored is an opportunity to explore the Christian
+                faith in a relaxed atmosphere. It is an informal 7 week course
+                that gets to the heart of what Christianity is all about and
+                explains what it means to become a Christian.
+              </p>
+              <h2>Who is it for?</h2>
+              <p>
+                Christianity Explored is for anyone who wants to know more about
+                Christianity. Maybe you were brought up going to church but never
+                really knew what it was all about. Maybe you have a Christian
+                friend or family member and you want to know more about what they
+                believe. Maybe the pandemic has got you wondering whether there\'s
+                more to life than just staying alive. If you have questions this
+                is the place for you.
+              </p>
+              <h2>What does it involve?</h2>
+              <p>
+                Christianity Explored provides the opportunity to ask questions.
+                Each week we'll hear a video about a particular aspect of the
+                Christian faith, which we'll discuss together. We'll then read a
+                short passage from the Bible, which we'll also discuss together.
+                It's a very relaxed course, so we can talk about any question
+                you may have, and discuss as much or as little as you're
+                comfortable with.
+              </p>
+              <h2>When does the course start?</h2>
+              <p>
+                The course launches after Easter on Thursday 8 April and will
+                run for 7 weeks. Each week we'll start at 7:30pm and sessions
+                will last up to 90 minutes.
+              </p>
+              <h2>Where will the course be held?</h2>
+              <p>
+                For obvious reasons we can't meet in homes like normal. We'll
+                be holding this course online via Zoom. If you register below
+                we'll email you the Zoom link to join us on 8 April.
+              </p>
+              <h2>Will I be embarrassed or pressured?</h2>
+              <p>
+                Don't worry - you won't be asked to read, pray or sing.
+                We certainly don't want to pressure people into becoming
+                Christians or joining the church.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row justify-content-md-center">
+          <div class="col-md-12 mb-5">
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/6DL_GO1HFA4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+
+        <div class="row justify-content-md-center">
+          <div class="col-md-8">
+            <div class="text-center text-white">
+              <h2>
+                Sign up now:
+              </h2>
+              <div class="card">
+                <div class="card-body">
+                  <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdowGddV8FJlPztioR7HYu2hrGP8sDtN9n-CTYyB7LNotjPWQ/viewform?embedded=true" width="640" height="996" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+  <!-- 2019: <main id="christmas-page">
+    <div class="christmas-head text-white">
+      <div class="container">
+        <h1 class="text-center">
+          Christmas at Crockenhill Baptist Church
+        </h1>
+      </div>
+    </div>
+
+    <section class="white-background pt-5">
+      <div class="container">
+        <h2 class="text-center">
+          Adventurers' presentation
+        </h2>
+        <p class="text-center">
+          A Christmas presentation by
+          <a href="/community/adventurers">Adventurers</a>,
+          our group for 6-9 year olds.
+          Everyone is welcome, but families of children in Adventurers are especially invited.
+        </p>
+        <h3 class="text-center">
+          Sunday 13 December, 10:30-11:30
+        </h3>
+      </div>
+    </section>
+
+    <section class="white-background pt-5">
+      <div class="container">
+        <h2 class="text-center">
+          Carols at the Chequers
+        </h2>
+        <p class="text-center">
+          An evening of Christmas carol singing in the village pub,
+          at the kind invitation of the landlord.
+        </p>
+        <h3 class="text-center">
+          Wednesday 16 December, from 7:30pm
+        </h3>
+      </div>
+    </section>
+
+    <section class="white-background pt-5">
+      <div class="container">
+        <h2 class="text-center">
+          Carols by Candlelight
+        </h2>
+        <p class="text-center">
+          A candlelit service of traditional Christmas carols and readings,
+          followed by refreshments.
+        </p>
+        <h3 class="text-center">
+          Sunday 20 December, 6:00-7:00pm
+        </h3>
+      </div>
+    </section>
+
+    <section class="white-background pt-5">
+      <div class="container">
+        <h2 class="text-center">
+          Christmas Day Celebration
+        </h2>
+        <p class="text-center">
+          We meet together on Christmas day to joyfully celebrate the birth of our Saviour - the almighty God born as a baby in Bethlehem.
+          Join us as we give glory to the new-born King.
+        </p>
+        <h3 class="text-center">
+          Friday 25 December, 10:00-10:45am
+        </h3>
+      </div>
+    </section>
+
+  </main> -->
+
+@stop
