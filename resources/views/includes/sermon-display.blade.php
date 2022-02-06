ec397d73c8f2dcfd4e6b73a0981a9f221adbd02a
Gareth Clarridge <gareth.clarridge@outlook.com>
2022-02-06 00:04:08 +0000

    Sermon series icons

--- resources/views/includes/sermon-display.blade.php
+++ resources/views/includes/sermon-display.blade.php
@@ -1,57 +1,76 @@
 <div class="card p-0">
-  <div class="card-body text-center">
-    <div class="card-title">
-      <h3>
-        <a href="/church/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">{{$sermon->title}}</a>
-      </h3>
-    </div>
-    <ul class="list-group list-group-flush">
-      @if (($sermon->date != null))
-        <li class="list-group-item">
-          <i class="far fa-calendar"></i>
-          &nbsp; {{ date('j F Y', strtotime($sermon->date)) }}
-        </li>
-      @endif
-      @if ($sermon->service != null)
-        <li class="list-group-item">
-          <i class="far fa-clock"></i>
-          &nbsp; {{ \Illuminate\Support\Str::title($sermon->service) }}
-        </li>
-      @endif
-      @if ($sermon->preacher != null)
-        <li class="list-group-item">
-          <i class="far fa-user"></i> &nbsp
-          <a href="/church/sermons/preachers/{{ \Illuminate\Support\Str::slug($sermon->preacher) }}">{{ $sermon->preacher }}</a>
-        </li>
-      @endif
-      @if ($sermon->series != null)
-        <li class="list-group-item">
-          <i class="fas fa-tag"></i> &nbsp
-          <a href="/church/sermons/series/{{ \Illuminate\Support\Str::slug($sermon->series) }}">{{ $sermon->series }}</a>
-        </li>
-      @endif
-      @if ($sermon->reference != null)
-        <li class="list-group-item">
-          <i class="fas fa-book"></i> &nbsp
-          {{ $sermon->reference }}
-        </li>
-      @endif
-    </ul>
-    @can ('edit-sermons')
-      <form method="POST" action="/church/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/delete" accept-charset="UTF-8" class="pt-3">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="btn-group">
-          <a href="/church/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/edit" class="btn btn-primary">
-            <i class="fas fa-pencil-alt"></i> &nbsp
-            Edit
-          </a>
+  <div class="row g-0">
+    @if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/images/sermons/' . $sermon->series . '.png'))
+      <div class="sermon-icon col-md-6 ratio ratio-1x1">
+        <img src="/images/sermons/{{$sermon->series}}.jpg" alt="{{$sermon->series}}">
+      </div>
+    @else
+      <div class="sermon-icon col-md-6 d-flex align-items-center justify-content-center ratio ratio-1x1" style="background-image: url('/images/sermons/standard.jpg'); background-size:100% auto;">
+        <span class="h1 text-white mx-auto align-middle text-center d-flex align-items-center justify-content-center px-5">
+          {{$sermon->series}}
+        </span>
+      </div>
+    @endif
+    <div class="col-md-6">
+      <div class="card-body text-center">
+        <ul class="list-group list-group-flush">
+          @if (($sermon->title != null))
+            <li class="list-group-item">
+              <h3>
+                <a class="my-auto" href="/church/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}">
+                  {{$sermon->title}}
+                </a>
+              </h3>
+            </li>
+          @endif
+          @if (($sermon->date != null))
+            <li class="list-group-item">
+              <i class="far fa-calendar"></i>
+              &nbsp; {{ date('j F Y', strtotime($sermon->date)) }}
+            </li>
+          @endif
+          @if ($sermon->service != null)
+            <li class="list-group-item">
+              <i class="far fa-clock"></i>
+              &nbsp; {{ \Illuminate\Support\Str::title($sermon->service) }}
+            </li>
+          @endif
+          @if ($sermon->preacher != null)
+            <li class="list-group-item">
+              <i class="far fa-user"></i> &nbsp
+              <a href="/church/sermons/preachers/{{ \Illuminate\Support\Str::slug($sermon->preacher) }}">{{ $sermon->preacher }}</a>
+            </li>
+          @endif
+          @if ($sermon->series != null)
+            <li class="list-group-item">
+              <i class="fas fa-tag"></i> &nbsp
+              <a href="/church/sermons/series/{{ \Illuminate\Support\Str::slug($sermon->series) }}">{{ $sermon->series }}</a>
+            </li>
+          @endif
+          @if ($sermon->reference != null)
+            <li class="list-group-item">
+              <i class="fas fa-book"></i> &nbsp
+              {{ $sermon->reference }}
+            </li>
+          @endif
+        </ul>
+        @can ('edit-sermons')
+          <form method="POST" action="/church/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/delete" accept-charset="UTF-8" class="pt-3">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="btn-group">
+              <a href="/church/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}/edit" class="btn btn-primary">
+                <i class="fas fa-pencil-alt"></i> &nbsp
+                Edit
+              </a>
 
-          <button type="submit" class="btn btn-danger">
-            <i class="fas fa-trash"></i> &nbsp
-            Delete
-          </button>
-        </div>
-      </form>
-    @endcan
+              <button type="submit" class="btn btn-danger">
+                <i class="fas fa-trash"></i> &nbsp
+                Delete
+              </button>
+            </div>
+          </form>
+        @endcan
+      </div>
+    </div>
   </div>
 </div>
