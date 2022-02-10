a7feb1caf0933f8f9a2d1d57158806c7849a2bfb
Gareth Clarridge <gareth.clarridge@outlook.com>
2022-02-10 23:36:34 +0000

    More RSS feed changes

--- resources/views/rss/eveningFeed.blade.php
+++ resources/views/rss/eveningFeed.blade.php
@@ -5,34 +5,52 @@
     <channel>
         <title>Sunday evenings at Crockenhill Baptist Church</title>
         <itunes:owner>
-            <itunes:email>admin@crockenhill.org</itunes:email>
+          <itunes:name>Crockenhill Baptist Church</itunes:name>
+          <itunes:email>admin@crockenhill.org</itunes:email>
         </itunes:owner>
         <itunes:author>Crockenhill Baptist Church</itunes:author>
         <link>http://crockenhill.org/church/sermons/evening</link>
         <itunes:summary>Sermons from Sunday evenings at Crockenhill Baptist Church</itunes:summary>
+        <description>Sermons from Sunday evenings at Crockenhill Baptist Church</description>
         <itunes:category text="Religion &amp; Spirituality">
           <itunes:category text="Christianity" />
           <itunes:category text="Religion" />
         </itunes:category>
         <itunes:explicit>no</itunes:explicit>
+        <image>
+          <link>http://crockenhill.org/church/sermons/evening</link>
+          <title>Crockenhill Baptist Church: sermons from the evening service</title>
+          <url>http://crockenhill.org/public/images/podcast/EveningArtwork.jpg</url>
+        </image>
         <itunes:image href="http://crockenhill.org/public/images/podcast/EveningArtwork.jpg"/>
+        <itunes:new-feed-url>http://crockenhill.org/church/sermons/evening/feed</itunes:new-feed-url>
         <language>en-gb</language>
         <pubDate>{{ now() }}</pubDate>
 
         @foreach($sermons as $sermon)
             <item>
+                <title><![CDATA[{{ $sermon->title }}]]></title>
                 <itunes:title><![CDATA[{{ $sermon->title }}]]></itunes:title>
                 <itunes:author>Crockenhill Baptist Church</itunes:author>
                 <link>http://crockenhill.org/church/sermons/{{date('Y', strtotime($sermon->date))}}/{{date('m', strtotime($sermon->date))}}/{{$sermon->slug}}</link>
                 <itunes:summary>
                   A sermon on <![CDATA[{!! $sermon->reference !!}]]> from <![CDATA[{!! $sermon->preacher !!}]]> as part of our <![CDATA[{!! $sermon->series !!}]]> series.
                 </itunes:summary>
+                <itunes:subtitle>
+                  A sermon on <![CDATA[{!! $sermon->reference !!}]]> from <![CDATA[{!! $sermon->preacher !!}]]> as part of our <![CDATA[{!! $sermon->series !!}]]> series.
+                </itunes:subtitle>
+                <itunes:description>
+                  A sermon on <![CDATA[{!! $sermon->reference !!}]]> from <![CDATA[{!! $sermon->preacher !!}]]> as part of our <![CDATA[{!! $sermon->series !!}]]> series.
+                </itunes:description>
                 <enclosure url="http://crockenhill.org/media/sermons/{{ $sermon->filename }}.mp3"
                            type="audio/mpeg" length="{{ $sermon->duration }}"/>
                 <itunes:duration>{{ $sermon->duration }}</itunes:duration>
                 <guid isPermaLink="false">{{ $sermon->id }}</guid>
                 <pubDate>{{ $sermon->created_at }}</pubDate>
+                <itunes:explicit>no</itunes:explicit>
+                <itunes:episodeType>full</itunes:episodeType>
             </item>
         @endforeach
+
     </channel>
 </rss>
