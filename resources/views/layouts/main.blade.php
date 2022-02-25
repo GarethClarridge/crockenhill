877d03e4928ced008b86e53a32ad5df35190054a
Gareth Clarridge <gareth.clarridge@outlook.com>
2022-02-25 21:53:22 +0000

    Moved fonts before css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,12 +12,13 @@
 
   @section('description') {{isset($description) ? $description : '<meta name="description" content="Crockenhill Baptist Church">'}} @endsection
 
+  <link href='https://fonts.googleapis.com/css2?family=Lato|Oswald' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
   <link type="text/css" rel="stylesheet" href="/stylesheets/main.css?v=7.1.1">
   <link type="text/css" rel="stylesheet" href="/stylesheets/print.css" media="print">
   @stack('styles')
 
-  <link href='https://fonts.googleapis.com/css2?family=Lato|Oswald' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=GvJNbAA7Wv">
   <link rel="icon" type="image/png" href="/favicon-32x32.png?v=GvJNbAA7Wv" sizes="32x32">
   <link rel="icon" type="image/png" href="/favicon-16x16.png?v=GvJNbAA7Wv" sizes="16x16">
