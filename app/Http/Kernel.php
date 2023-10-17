db02163ba16a4fc881edf338b2d0888e8d18ef08
Gareth Clarridge <gareth.clarridge@outlook.com>
2023-10-17 21:20:47 +0100

    Updated to laravel 10

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \Crockenhill\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
