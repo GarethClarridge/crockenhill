26cffcf8bbac98ce03668f8e6998e9f34b6e6504
Gareth Clarridge <gareth.clarridge@outlook.com>
2023-10-17 21:20:47 +0100

    Updated to laravel 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,8 +21,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
         \Gate::before(function ($user, $ability) {
             if ($user->email === "admin@crockenhill.org") {
                 return true;
