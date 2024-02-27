4aa56e7de00a3f755438b15725314db51124c156
Gareth Clarridge <gareth.clarridge@outlook.com>
2024-02-27 20:42:51 +0000

    Family Talk

--- resources/views/full-width-pages/community.blade.php
+++ resources/views/full-width-pages/community.blade.php
@@ -1,103 +1,105 @@
 @extends('layouts.main')
 
 @section('title')
-  Community
+Community
 @stop
 
 @section('description')
-  Community
+Community
 @stop
 
 @section('content')
-  <main>
+<main>
 
-    <x-h1>
-      Community
-    </x-h1>
+  <x-h1>
+    Community
+  </x-h1>
 
-    <x-text>
-      <p>
-        We're a local church - in Crockenhill, for the people of
-        Crockenhill. We want to use the gifts God has given us to
-        serve our community.
-      </p>
-      <p>
-        We believe the best way we can help people is by making
-        <a href="/christ">the good news about Jesus Christ</a> known
-        to everyone in Crockenhill and the surrounding area, although
-        we're also keen to help meet people's physical needs where we
-        can.
-      </p>
-      <p>
-        Our activities are open and welcoming to everyone: whether
-        you're a committed Christian or just someone who wants a cup
-        of coffee and a chat with local people.
-      </p>
-    </x-text>
+  <x-text>
+    <p>
+      We're a local church - in Crockenhill, for the people of
+      Crockenhill. We want to use the gifts God has given us to
+      serve our community.
+    </p>
+    <p>
+      We believe the best way we can help people is by making
+      <a href="/christ">the good news about Jesus Christ</a> known
+      to everyone in Crockenhill and the surrounding area, although
+      we're also keen to help meet people's physical needs where we
+      can.
+    </p>
+    <p>
+      Our activities are open and welcoming to everyone: whether
+      you're a committed Christian or just someone who wants a cup
+      of coffee and a chat with local people.
+    </p>
+  </x-text>
 
-    <div class="grid grid-cols-1 gap-3 mx-12 my-6">
-      <x-button link="#i-want-to-meet-local-people">
-        I want to meet local people
-      </x-button>
-      <x-button link="#ive-got-children">
-        I've got children
-      </x-button>
-      <x-button link="#i-want-to-find-out-more-about-jesus">
-        I want to find out more about Jesus
-      </x-button>
-    </div>
-
-    <x-h2>
+  <div class="grid grid-cols-1 gap-3 mx-12 my-6">
+    <x-button link="#i-want-to-meet-local-people">
       I want to meet local people
-    </x-h2>
-
-    <div class="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-center max-w-2xl lg:max-w-5xl mx-auto mt-6">
-          
-      <x-page-card :page="$pages->firstWhere('slug', 'coffee-cup')" />
-
-      <x-page-card :page="$pages->firstWhere('slug', 'baby-talk')" />
-        
-      <x-page-card :page="$pages->firstWhere('slug', 'sunday-mornings')" />
-        
-    </div>
-
-
-    <x-h2>
+    </x-button>
+    <x-button link="#ive-got-children">
       I've got children
-    </x-h2>
-
-    <div class="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-center max-w-2xl lg:max-w-5xl mx-auto mt-6">
-          
-      <x-page-card :page="$pages->firstWhere('slug', 'baby-talk')" />
-                
-    </div>
-
-
-    <x-h2> 
+    </x-button>
+    <x-button link="#i-want-to-find-out-more-about-jesus">
       I want to find out more about Jesus
-    </x-h2>
+    </x-button>
+  </div>
 
-    <div class="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-center max-w-2xl lg:max-w-5xl mx-auto mt-6">
-          
-      <x-page-card :page="$pages->firstWhere('slug', 'sunday-mornings')" />
+  <x-h2>
+    I want to meet local people
+  </x-h2>
 
-      <x-page-card :page="$pages->firstWhere('slug', 'christianity-explored')" />
-        
-      <x-page-card :page="$pages->firstWhere('slug', 'bible-study')" />
-        
-    </div>
+  <div class="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-center max-w-2xl lg:max-w-5xl mx-auto mt-6">
+
+    <x-page-card :page="$pages->firstWhere('slug', 'coffee-cup')" />
+
+    <x-page-card :page="$pages->firstWhere('slug', 'baby-talk')" />
+
+    <x-page-card :page="$pages->firstWhere('slug', 'sunday-mornings')" />
+
+  </div>
 
 
-    <x-h2>
-      Occasional events
-    </x-h2>
+  <x-h2>
+    I've got children
+  </x-h2>
 
-    <div class="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-center max-w-2xl lg:max-w-5xl mx-auto mt-6">
+  <div class="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-center max-w-2xl lg:max-w-5xl mx-auto mt-6">
 
-      <x-page-card :page="$pages->firstWhere('slug', 'carols-in-the-chequers')" />
-        
-    </div>
+    <x-page-card :page="$pages->firstWhere('slug', 'baby-talk')" />
 
-  </main>
+    <x-page-card :page="$pages->firstWhere('slug', 'family-talk')" />
+
+  </div>
+
+
+  <x-h2>
+    I want to find out more about Jesus
+  </x-h2>
+
+  <div class="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-center max-w-2xl lg:max-w-5xl mx-auto mt-6">
+
+    <x-page-card :page="$pages->firstWhere('slug', 'sunday-mornings')" />
+
+    <x-page-card :page="$pages->firstWhere('slug', 'christianity-explored')" />
+
+    <x-page-card :page="$pages->firstWhere('slug', 'bible-study')" />
+
+  </div>
+
+
+  <x-h2>
+    Occasional events
+  </x-h2>
+
+  <div class="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-center max-w-2xl lg:max-w-5xl mx-auto mt-6">
+
+    <x-page-card :page="$pages->firstWhere('slug', 'carols-in-the-chequers')" />
+
+  </div>
+
+</main>
 
 @stop
