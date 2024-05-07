b4ae1c0b85b263e9debf472c1a7e0b818351a256
Gareth Clarridge <gareth.clarridge@outlook.com>
2024-05-07 20:20:06 +0100

    Fixed heading photos

--- resources/views/components/h1-picture.blade.php
+++ resources/views/components/h1-picture.blade.php
@@ -1,6 +1,5 @@
-<div class="flex items-center justify-center overflow-hidden h-[33vh]"
-        style="background:linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ),url('/images/headings/small/sunday-mornings.jpg') center/cover no-repeat;">
-    <h1 class="text-white font-display text-6xl">
-        {{ $slot }}
-    </h1>
+<div class="flex items-center justify-center overflow-hidden h-[33vh]" style="background:linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ),url('{{$headingpicture}}') center/cover no-repeat;">
+  <h1 class="text-white font-display text-6xl">
+    {{ $slot }}
+  </h1>
 </div>
