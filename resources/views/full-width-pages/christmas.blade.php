eb1e72f52b2fba683be76beb2f1bc85cfee2627c
Gareth Clarridge <gareth.clarridge@outlook.com>
2024-11-28 22:23:10 +0000

    Christmas 2024

---
+++ resources/views/full-width-pages/christmas.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.main')
+
+@section('title')
+Christmas
+@stop
+
+@section('description')
+Christmas
+@stop
+
+@section('content')
+<main class="-mb-10 bg-cover bg-center bg-[url('/public/images/homepage/christmas2023.jpg')] bg-gray-700 bg-blend-multiply">
+
+  <x-h1>
+    <span class="text-white">Christmas at Crockenhill Baptist Church</span>
+  </x-h1>
+
+  <x-text>
+    <p class="text-white">
+      Celebrate the birth of Jesus Christ with us this Christmas.
+    </p>
+  </x-text>
+
+  <div class="mx-auto max-w-screen-xl text-center pt-24 pb-12">
+    <h3 class="font-display text-white mt-8 text-xl lg:text-2xl sm:px-16 lg:px-48">
+      Preparing Room
+    </h3>
+    <p class="mb-8 text-lg text-white font-normal lg:text-xl sm:px-16 lg:px-48">
+      Saturday 30th November, 3-6pm
+    </p>
+    <h3 class="font-display text-white mt-8 text-xl lg:text-2xl sm:px-16 lg:px-48">
+      Coffee Cup Carols
+    </h3>
+    <p class="mb-8 text-lg text-white font-normal lg:text-xl sm:px-16 lg:px-48">
+      Thursday 12th, 10:30am
+    </p>
+    <h3 class="font-display text-white mt-8 text-xl lg:text-2xl sm:px-16 lg:px-48">
+      Carols in the Chequers
+    </h3>
+    <p class="mb-8 text-lg text-white font-normal lg:text-xl sm:px-16 lg:px-48">
+      Wednesday 18th, 7:30pm
+    </p>
+    <h3 class="font-display text-white mt-8 text-xl lg:text-2xl sm:px-16 lg:px-48">
+      Carols by Candlelight
+    </h3>
+    <p class="mb-8 text-lg text-white font-normal lg:text-xl sm:px-16 lg:px-48">
+      Sunday 22nd, 6:00pm
+    </p>
+    <h3 class="font-display text-white mt-8 text-xl lg:text-2xl sm:px-16 lg:px-48">
+      Christmas Morning Service
+    </h3>
+    <p class="mb-8 text-lg text-white font-normal lg:text-xl sm:px-16 lg:px-48">
+      Wednesday 25th, 10:30am
+    </p>
+  </div>
+
+</main>
+
+@stop
