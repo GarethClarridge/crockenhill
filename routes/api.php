558d7f773846109727ad576307462a6f8d7306d2
Gareth Clarridge <gareth.clarridge@outlook.com>
2025-06-14 07:20:41 +0100

    Fixed route file

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => 'api'], function () {
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
-        return $request->user();
-    });
+  Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+  });
 });
