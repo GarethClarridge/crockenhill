6a9dc36015e5c2ceeb7481b819c63b819605ac36
Gareth Clarridge <gareth.clarridge@outlook.com>
2025-09-06 23:57:02 +0100

    Users migration hack

--- database/migrations/2025_06_29_151500_fix_users_table_timestamps.php
+++ database/migrations/2025_06_29_151500_fix_users_table_timestamps.php
@@ -3,22 +3,19 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('created_at')->nullable()->default(null)->change();
-            $table->timestamp('updated_at')->nullable()->default(null)->change();
-        });
+        DB::statement("ALTER TABLE users MODIFY created_at TIMESTAMP NULL DEFAULT NULL");
+        DB::statement("ALTER TABLE users MODIFY updated_at TIMESTAMP NULL DEFAULT NULL");
     }
 
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('created_at')->nullable(false)->change();
-            $table->timestamp('updated_at')->nullable(false)->change();
-        });
+        DB::statement("ALTER TABLE users MODIFY created_at TIMESTAMP NOT NULL");
+        DB::statement("ALTER TABLE users MODIFY updated_at TIMESTAMP NOT NULL");
     }
 };
